66dbeae37965d8b244018282de80b39cbaed982c
오완진 <dhkswls1998@naver.com>
2024-07-26 17:41:57 +0900

    240726

--- practice/src/baekjoon/Main.java
+++ practice/src/baekjoon/Main.java
@@ -1,13 +1,37 @@
 package baekjoon;
+
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		int ch = sc.next().charAt(0);
+
+		int T = sc.nextInt();
 		
-		System.out.println(ch);
+		for (int i = 0; i < T; i++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			int tmp = N;
+			if (M > N) {
+				N = M;
+				M = tmp;
+			} else if (M == N) {
+				System.out.println(1);
+				continue;
+			}
+			
+			long cnt = 1;
+			for (int j = N; j >= N-M+1; j--) {
+				cnt *= j;
+			}
+			for (int j = M; j >= 1; j--) {
+				cnt /= j;
+			}
+			System.out.println(cnt);
+						
+		}
+		
 	}
 }
