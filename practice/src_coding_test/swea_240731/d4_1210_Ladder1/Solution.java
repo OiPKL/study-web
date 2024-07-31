0d4c8b0aa87e0c4e94f3e04f01ee04f579d2ee49
oipkl <dhkswls1998@naver.com>
2024-08-01 07:58:43 +0900

    24731

--- practice/src_coding_test/swea_240731/d4_1210_Ladder1/Solution.java
+++ practice/src_coding_test/swea_240731/d4_1210_Ladder1/Solution.java
@@ -10,63 +10,42 @@ public class Solution {
 
 		for (int t = 1; t <= 10; t++) {
 			
-			// 델타배열 : 상(0) 좌(1) 우(2)
-			int[] rWay = {-1, 0, 0};
-			int[] cWay = {0, -1, 1};
-			int way = 0;
-			int rNow = 0;
-			int cNow = 0;
-			int rNext = 0;
-			int cNext = 0;
-			int X1 = 0;
-			int X2 = 0;
-			
 			int T = sc.nextInt();
-			int[][] ladder = new int[100][100];
+			int[][] sadari = new int[100][100];
 			
 			for (int r = 0; r < 100; r++) {
 				for (int c = 0; c < 100; c++) {
-					ladder[r][c] = sc.nextInt();
+					sadari[r][c] = sc.nextInt();
 				}
 			}
 			
-			// X2 확인 ~ 역주행 ~ X1 찾기
+			// end 찾기 ~ 역주행 ~ start 찾기
+			int end = -1;
 			for (int c = 0; c < 100; c++) {
-				if (ladder[99][c] == 2)
-					X2 = c;
-			}
-			
-			rNow = 99;
-			cNow = X2;
-			
-			while (true) {
-				
-				if (rNow == 0) {
-					X1 = cNow;
+				if (sadari[99][c] == 2) {
+					end = c;
 					break;
 				}
-				
-				way = 0; 			// 상(기본)
-				rNext = rNow + rWay[way];
-				cNext = cNow + cWay[way];
-				
-				// 방향 설정 (Short Circuit 사용)
-				if (cNext > 0 && ladder[rNext][cNext] == 0) {
-					way = 1; 		// 좌
-					rNext = rNow + rWay[way];
-					cNext = cNow + cWay[way];
-					if (cNext < 99 && ladder[rNext][cNext] == 0) {
-						way = 2;	// 우
-						rNext = rNow + rWay[way];
-						cNext = cNow + cWay[way];
-					}
-				}
-				
-				rNow += rWay[way];
-				cNow += cWay[way];
 			}
 			
-			System.out.println("#" + T + " " + X1);
+			int rNow = 99;
+			int cNow = end;
+			
+			while (rNow > 0) {
+				// 좌측 탐색 후 좌로 전진
+				if (cNow > 0 && sadari[rNow][cNow - 1] == 1) {
+					while (cNow > 0 && sadari[rNow][cNow - 1] == 1)
+						cNow--;
+				// 우측 탐색 후 우로 전진
+				} else if (cNow < 99 && sadari[rNow][cNow + 1] == 1) {
+					while (cNow < 99 && sadari[rNow][cNow + 1] == 1)
+						cNow++;
+				} 
+				// 위로 전진
+				rNow--;
+			}
+			
+			System.out.println("#" + T + " " + cNow);
 		}
 		
 	}
