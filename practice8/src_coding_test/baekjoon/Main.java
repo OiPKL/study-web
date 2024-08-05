0779c0fbd6529e72ae1b5f5e0c5788b9aa365b79
오완진 <dhkswls1998@naver.com>
2024-08-05 18:39:36 +0900

    240805

--- practice8/src_coding_test/baekjoon/Main.java
+++ practice8/src_coding_test/baekjoon/Main.java
@@ -7,6 +7,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         
         int N = sc.nextInt();
+        sc.nextLine();
+        
         String candy = "";
         char[][] candyArr = new char[N][N];
         
@@ -14,8 +16,42 @@ public class Main {
         	candy = sc.nextLine();
         	candyArr[r] = candy.toCharArray();
         }
+        
+//        System.out.println(N);
+//        for (int r = 0; r < N; r++) {
+//        	for (int c = 0; c < N; c++) {
+//        		System.out.print(candyArr[r][c]);
+//        	}
+//        	System.out.println();
+//        }
 
+        int cnt = 0;
+        int max = 0;
         
+        // 행 탐색
+        for (int r = 0; r < N; r++) {
+        	for (int c = 0; c < N - 1; c++) {
+        		cnt = 0;
+        		if (candyArr[r][c] == candyArr[r][c+1]) {
+        			while (candyArr[r][c] == candyArr[r][c+1])
+        				cnt++;
+        		}
+        		max = Math.max(max, cnt);
+        	}
+        }
         
+        // 열 탐색
+    	for (int c = 0; c < N; c++) {
+    		for (int r = 0; r < N - 1; r++) {
+    			cnt = 0;
+        		if (candyArr[r][c] == candyArr[r+1][c]) {
+        			while (candyArr[r][c] == candyArr[r+1][c])
+        				cnt++;
+        		}
+        		max = Math.max(max, cnt);
+        	}
+        }
+        
+    	System.out.println(max);
     }
 }
--- practice8/src_coding_test/swea/Solution.java
+++ practice8/src_coding_test/swea/Solution.java
@@ -1,41 +1,120 @@
 package swea;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Solution {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		
-		for (int t = 1; t <= T; t++) {
-			
-			int K = sc.nextInt();
-			List<Integer> arr = new ArrayList<>();
-			
-			for (int k = 0; k < K; k++) {
-				arr.add(sc.nextInt());
-			}
 
-			int cnt = 0;
-			for (int k = 0; k < K; k++) {
-				if (arr.get(k) == 0) cnt++;
-			}
-			
-			for (int c = 0; c < cnt; c++) {
-				arr.remove(arr.size() - 1);
-			}
-			
-			if (arr.size() == 0)
-				// 다 지워지는 경우는 마지막이 0인 경우 뿐임
-				System.out.println("#" + t + " " + 0);
-			else
-				System.out.println("#" + t + " " + (arr.get(arr.size() - 1)));
-		}
-		
-	}	
+    public static int checkBracket(Stack<Character> stack, char bracket) {
+        if (bracket == '(' || bracket == '{' || bracket == '[' || bracket == '<') {
+            stack.push(bracket);
+            return 1;
+        }
+        
+        if (stack.isEmpty()) return 0;
+        
+        char top = stack.pop();
+        if ((bracket == ')' && top != '(') ||
+            (bracket == '}' && top != '{') ||
+            (bracket == ']' && top != '[') ||
+            (bracket == '>' && top != '<')) {
+            return 0;
+        }
+        
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        for (int t = 1; t <= 10; t++) {
+            int isValid = 1;
+            int N = sc.nextInt();
+            sc.nextLine();
+            String str = sc.nextLine();
+            char[] charArr = str.toCharArray();
+            Stack<Character> stack = new Stack<>();
+
+            for (char c : charArr) {
+                isValid = checkBracket(stack, c);
+                if (isValid == 0) break;
+            }
+
+            if (!stack.isEmpty()) isValid = 0;
+
+            System.out.println("#" + t + " " + isValid);
+        }
+        
+        sc.close();
+    }
 }
+
+//public class Solution {
+//
+//	public static int checkBracket(Stack<Character> stack, char bracket) {
+//		
+//		switch (bracket) {
+//			case '(': 	stack.push(bracket);
+//													return 1;
+//			case ')': 	if (stack.pop() == '(') {	return 1;
+//						} else 						return 0;
+//			case '{': 	stack.push(bracket);
+//													return 1;
+//			case '}': 	if (stack.pop() == '{') {	return 1;
+//						} else 						return 0;
+//			case '[': 	stack.push(bracket);
+//													return 1;
+//			case ']': 	if (stack.pop() == '[') {	return 1;
+//						} else 						return 0;
+//			case '<': 	stack.push(bracket);
+//													return 1;
+//			case '>': 	if (stack.pop() == '<') {	return 1;
+//						} else 						return 0;
+//			default : 								return 1;
+//		}
+//		
+//	}
+//	
+//	public static void main(String[] args) {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		Stack<Character> stack = new Stack<>();
+//		
+//		for (int t = 1; t <= 10; t++) {
+//			
+//			int isValid = 1;
+//			int N = sc.nextInt();
+//			sc.nextLine();
+//			String str = sc.nextLine();
+//			char[] charArr = str.toCharArray();
+//			
+////			for (char arr : charArr)
+////				System.out.print(arr + " ");
+//
+//			int n = 0;
+//			while (isValid == 1) {
+//				if (stack.size() == 0 && charArr[n] == ')') {
+//					isValid = 0;
+//					break;
+//				}
+//				else if (stack.size() == 0 && charArr[n] == '}') {
+//					isValid = 0;
+//					break;
+//				}
+//				else if (stack.size() == 0 && charArr[n] == ']') {
+//					isValid = 0;
+//					break;
+//				}
+//				else if (stack.size() == 0 && charArr[n] == '>') {
+//					isValid = 0;
+//					break;
+//				}
+//				isValid = checkBracket(stack, charArr[n++]);
+//			}
+//			if (stack.size() > 0) isValid = 0;
+//			System.out.println("#" + t + " " + isValid);
+//
+//		}
+//			
+//	}
+//}
