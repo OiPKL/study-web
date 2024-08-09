ef7284d7397ce610cc693dbc53debae1cdd0b9b9
오완진 <dhkswls1998@naver.com>
2024-08-09 18:45:18 +0900

    240809

--- practice8/src_coding_test/baekjoon/Main.java
+++ practice8/src_coding_test/baekjoon/Main.java
@@ -8,65 +8,50 @@ public class Main {
     	
     	int N = sc.nextInt();
     	boolean[][] chess = new boolean[N][N];
+    	// 체스판: false / 퀸: true
+    	
+    	setQueen(chess, 0, 0);
 
     	System.out.println(cnt);
     		
     }
     
-    static void setQueen()
+    static int cnt = 0;
+    
+    static void setQueen(boolean[][] chess, int rQueen, int cQueen) {
+    	
+    	int N = chess.length;
+    	
+    	if (rQueen == N - 1) {
+    		cnt++;
+    		return;
+    	}
+    	
+    	for (int i = rQueen; i < N; i++) {
+    		rQueen++;
+    		setQueen(chess, rQueen, cQueen);
+    	}
+    	
+    }
+    
+    static void setNextQueen(boolean[][] chess, int rNowQueen, int cNextQueen) {
+
+    	int N = chess.length;
+    	
+		for (int c = 0; c < N; c++) {
+			// 가로
+			if (chess[rNowQueen][c] == false)
+			// 세로
+			for (int r = 0; r < N; r++) {
+				if (chess[r][c] == false)
+			}
+			
+			if (chess[r][c] == false)
+			// 대각선1
+			if ((rNowQueen + c) == (r))
+			// 대각선2
+		}
+    	
+    }
     
 }
-    
-//    static int cnt = 0;
-//    
-//    static boolean[][] setQueenZone(boolean[][] chess, int rQueen, int cQueen) {
-//    	
-//    	int N = chess.length;
-//    	
-//		chess[rQueen] = new boolean[N];	// 동서
-//    	for (int r = 0; r < N; r++)
-//    		chess[r][cQueen] = false;	// 남북
-//    	int r = rQueen;
-//    	int c = cQueen;
-//    	while (r < N && c < N)
-//    		chess[r++][c++] = false;	// 남동
-//    	r = rQueen;
-//    	c = cQueen;
-//    	while (r < N && c >= 0)
-//    		chess[r++][c--] = false;	// 남서
-//    	r = rQueen;
-//    	c = cQueen;
-//    	while (r >= 0 && c < N)
-//    		chess[r--][c++] = false;	// 북동
-//    	r = rQueen;
-//    	c = cQueen;
-//    	while (r >= 0 && c >= 0)
-//    		chess[r--][c--] = false;	// 북서
-//    	
-//    	return chess;
-//    	
-//    }
-//    
-//    static void setQueen(boolean[][] oldChess, boolean[][] newChess, int rQueen, int cQueen) {
-//    	
-//    	int N = oldChess.length;
-//    	
-//    	if (rQueen == N) {
-//    		cnt++;
-//    		return;
-//    	}
-//    	
-//    	for (int r = rQueen; r < N; r++) {
-//    		newChess = setQueenZone(oldChess, rQueen, cQueen);
-//    		int tmpQueen = cQueen;
-//    		for (int c = 0; c < N; c++) {
-//    			if (newChess[rQueen++][c] == true)
-//    				cQueen = c;				// 다음 cQueen 탐색
-//    			if (cQueen == tmpQueen)	{	// 조건 만족하는 cQueen 존재 x
-//    				newChess = oldChess;
-//    				setQueen(oldChess, newChess, rQueen, cQueen);
-//    			}
-//    		}
-//    	}
-//    	
-//    }
