453c6c777ca7c9253ef4864a46a96e185b0b6314
dhkswls1998 <dhkswls1998@naver.com>
2024-08-10 10:18:29 +0900

    240810

--- practice8/src_coding_test/baekjoon/Main.java
+++ practice8/src_coding_test/baekjoon/Main.java
@@ -1,5 +1,7 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,53 +9,63 @@ public class Main {
     	Scanner sc = new Scanner(System.in);
     	
     	int N = sc.nextInt();
-    	boolean[][] chess = new boolean[N][N];
     	// 체스판: false / 퀸: true
+    	boolean[][] chess = new boolean[N][N];
     	
-    	setQueen(chess, 0, 0);
+    	// chess[0][] 에서 시작 ~ 백트래킹 = 모든 경우의 수마다 가지를 친다(?) 느낌
+    	setQueen(chess, 0);
     	System.out.println(cnt);
     		
     }
     
     static int cnt = 0;
     
-    static void setQueen(boolean[][] chess, int rQueen, int cQueen) {
+    // rNextQueen(rQueen + 1) 에서 cNextQueensFinder 메서드로 cNextQueens 탐색
+    // chess[rQueen][cQueen] 을 true로 설정
+    // cNextQueens 에서 반복하면서 cnt++
+    static void setQueen(boolean[][] chess, int rQueen) {
     	
+    	// rQueen + 1 -> N
     	int N = chess.length;
     	
-    	if (rQueen == N - 1) {
+    	if (rQueen == N) {
     		cnt++;
     		return;
     	}
     	
-    	for (int i = rQueen; i < N; i++) {
-    		rQueen++;
-    		cQueen = setNextQueen(chess, rQueen);
-    		setQueen(chess, rQueen, cQueen);
-    		rQueen--;
+    	List<Integer> cNextQueens = cNextQueensFinder(chess, rQueen);
+
+    	for (int cQueen : cNextQueens) {
+    		chess[rQueen][cQueen] = true;
+    		setQueen(chess, rQueen + 1);
+    		chess[rQueen][cQueen] = false;
     	}
     	
     }
     
-    static boolean isSafe(boolean[][] chess, int rCheck, int cCheck) {
+    // rNextQueen 에서 놓일수 있는 cNextQueens 반환 메서드
+    static List<Integer> cNextQueensFinder(boolean[][] chess, int rNextQueen) {
 
     	int N = chess.length;
+    	List<Integer> cNextQueens = new ArrayList<>();
     	
-		for (int r = 0; r < N; r++) {
-			if (chess[r][cCheck]) return false;
-		}
-		// 대각선1 (\ : r+c 일치 확인) -> 행에 대각선 위치의 (r, cCheck)가 존재한다면 ~
-		for (int r = 0; r < N; r++) {
-			int cCheck = (rNext + c) - r;
-			if (cCheck >= 0 && cCheck < N && chess[r][cCheck] == true) continue outer;
-		}
-		// 대각선2 (/ : r-c 일치 확인) -> 행에 대각선 위치의 (r, cCheck)가 존재한다면 ~
-		for (int r = 0; r < N; r++) {
-			int cCheck = (c - rNext) + r;
-			if (cCheck >= 0 && cCheck < N && chess[r][cCheck] == true) continue outer;
-		}
-		int cNext = c;
-		return cNext;
+    	outer:
+    	for (int c = 0; c < N; c++) {				// cNextQueens 후보들
+    		for (int r = 0; r < rNextQueen; r++) {	// 0 ~ rNextQueen 에서 탐색
+    			// 가로 : rNextQueen에서 cNextQueen을 찾는거라 Pass
+    			// 세로
+    			if (chess[r][c]) continue outer;
+    			// 대각선(\) : r + cDaegak1(어딘지 모르는 대각선 상의 c) = rNextQueen + c(=cNextQueen)
+    			// 대각선(/) : r - cDaegak2(어딘지 모르는 대각선 상의 c) = rNextQueen - c(=cNextQueen)
+    			int cDaegak1 = (rNextQueen + c) - r;
+    			int cDaegak2 = r - (rNextQueen - c);
+    			if (cDaegak1 >= 0 && cDaegak1 < N && chess[r][cDaegak1]) continue outer;
+    			if (cDaegak2 >= 0 && cDaegak2 < N && chess[r][cDaegak2]) continue outer;
+    		}
+    		cNextQueens.add(c);
+    	}
+    	
+    	return cNextQueens;
     	
     }
     
