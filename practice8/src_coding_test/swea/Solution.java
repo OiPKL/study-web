0ecab42f6f0d4bf57712650217bd6f1139b387bd
dhkswls1998 <dhkswls1998@naver.com>
2024-08-11 15:55:28 +0900

    240811

--- practice8/src_coding_test/swea/Solution.java
+++ practice8/src_coding_test/swea/Solution.java
@@ -1,32 +1,69 @@
 package swea;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
+    static List<Long> note = new ArrayList<>();
+    static long sum = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int T = sc.nextInt();
-        sc.nextLine();
-        
-        for (int tc = 1; tc <= T; tc++) {
-        	
-        	char[] tmpArr = sc.nextLine().toCharArray();
-        	int N = tmpArr.length;
-        	int[] memory = new int[N];
-        	for (int n = 0; n < N; n++)
-        		memory[n] = (int)(tmpArr[n] - '0');
-        	int cnt = 1;
-        	
-        	// 뒤에서부터 0 - 1 - 0 바뀌는 순간 카운트
-        	// 맨 앞이 0이면 카운트--
-        	for (int n = N - 1; n > 0; n--) {
-        		if (memory[n] != memory[n-1]) cnt++;
-        	}
-        	if (memory[0] == 0) cnt--;
-        	
-        	System.out.println("#" + tc + " " + cnt);
+        int TC = sc.nextInt();
+
+        for (int tc = 1; tc <= TC; tc++) {
+            int N = sc.nextInt();
+            long A = sc.nextLong();
+
+            // 초기화
+            note.clear();
+            note.add(0L); // 0번 인덱스는 사용하지 않음
+            note.add(A);
+            sum = A;
+
+            for (int n = 0; n < N; n++) {
+                long b = sc.nextLong();
+                long c = sc.nextLong();
+
+                // 두 숫자를 추가
+                note.add(b);
+                note.add(c);
+
+                // 중간값 찾기
+                findMiddle(1);
+
+                // 중간값을 sum에 추가
+                sum += note.get(1);
+            }
+
+            System.out.println("#" + tc + " " + sum % 20171109);
+        }
+        sc.close();
+    }
+
+    static void findMiddle(int idx) {
+        if (idx * 2 >= note.size()) return;
+
+        // 왼쪽 자식과 오른쪽 자식의 인덱스
+        int leftIdx = idx * 2;
+        int rightIdx = idx * 2 + 1;
+
+        // 현재 부모 노드와 자식 노드들의 값을 가져옴
+        long parent = note.get(idx);
+        long leftChild = note.get(leftIdx);
+        long rightChild = (rightIdx < note.size()) ? note.get(rightIdx) : Long.MAX_VALUE;
+
+        // 정렬: leftChild <= parent <= rightChild
+        if (parent > rightChild && rightIdx < note.size()) {
+            note.set(idx, rightChild);
+            note.set(rightIdx, parent);
+            findMiddle(rightIdx);
+        }
+        if (parent < leftChild) {
+            note.set(idx, leftChild);
+            note.set(leftIdx, parent);
+            findMiddle(leftIdx);
         }
     }
-    
 }
