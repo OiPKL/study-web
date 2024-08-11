2658a2ca61a94347a04e3506a5d54629d2bc58b3
dhkswls1998 <dhkswls1998@naver.com>
2024-08-11 20:04:31 +0900

    240811

--- practice8/src_coding_test/programmers/Solution.java
+++ practice8/src_coding_test/programmers/Solution.java
@@ -1,34 +1,20 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-	static List<int[]> records = new ArrayList<>();
-	
-    public int[][] solution(int n) {
-
-    	hanoi(n, 1, 3, 2);
+    public int[] solution(int[] prices) {
     	
-    	int N = records.size();
-    	int[][] answer = new int[N][2];
+    	int N = prices.length;
+    	int[] times = new int[N];
     	
-    	int idx = 0;
-    	for (int[] record : records)
-    		answer[idx++] = record;
-    	
-    	return answer;
-    }
-
-    void hanoi(int n, int from, int to, int tmp) {
-    	if (n == 1) {
-    		records.add(new int[] {from, to});
-    		return;
+    	for (int i = 0; i < N; i++) {
+    		int cnt = 0;
+    		for (int j = i+1; j < N; j++) {
+    			cnt++;
+    			if (prices[i] > prices[j]) break;
+    		}
+    		times[i] = cnt;
     	}
-    		
-    	hanoi(n - 1, from, tmp, to);		// (n-1)개의 원판을 from -> tmp
-    	records.add(new int[] {from, to});	// n번 원판을 from -> to
-    	hanoi(n - 1, tmp, to, from);		// (n-1)개의 원판을 tmp -> to
+    	return times;
     }
 }
 
