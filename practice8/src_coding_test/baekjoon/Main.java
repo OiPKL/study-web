8bfb2eba7c57000c69dd17ca1d1f909c1a44eb3b
dhkswls1998 <dhkswls1998@naver.com>
2024-08-13 23:33:08 +0900

    240813

--- practice8/src_coding_test/baekjoon/Main.java
+++ practice8/src_coding_test/baekjoon/Main.java
@@ -1,39 +1,57 @@
 package baekjoon;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+	static int cnt = 0;
+	
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		decreasingNums.add(0);					// 첫째자리 0 * digit = 0 해결
-		getDecreasingNums(0, 10, 1, 1, N);		// preDigit = 10 : nextDigit = 0 ~ 9
-		if (cnt >= N) 	System.out.println(decreasingNums.get(N-1));
-		else 			System.out.println(-1);
-		
+    	// 체스판: false / 퀸: true
+    	boolean[][] chess = new boolean[N][N];
+    	
+    	// chess[0][] 시작
+    	setQueen(chess, 0);
+    	System.out.println(cnt);
 	}
 	
-	static int cnt = 1;
-	static List<Integer> decreasingNums = new ArrayList<>();
-											// 앞자리수		현재자리수			전체자리수
-	static void getDecreasingNums(int num, int preDigit, int currDigit, int totalDigit, int N) {
+	static void setQueen(boolean[][] chess, int rQueen) {
 		
-		for (int digit = 0; digit < preDigit; digit++) {
-			if (num == 0 && digit == 0) continue;
-			num = num*10 + digit;
-			decreasingNums.add(num);
+		int N = chess.length;
+		
+		// rQueen + 1 -> N - 1 (x)
+		if (rQueen == N) {
 			cnt++;
-			if (cnt >= N) return;
-			getDecreasingNums(num, digit, ++currDigit, totalDigit, N);
-			num /= 10;
+			return;
 		}
 		
-		getDecreasingNums(0, 10, 1, ++totalDigit, N);
-		
+		for (int cQueen = 0; cQueen < N; cQueen++) {
+			if (isSafe(chess, rQueen, cQueen)) {
+				chess[rQueen][cQueen] = true;	// 퀸 놓기
+				setQueen(chess, rQueen + 1);	// 다음 행
+				chess[rQueen][cQueen] = false;	// 퀸 제거
+			}
+		}
 	}
 	
+	static boolean isSafe(boolean[][] chess, int rQueen, int cQueen) {
+		
+		int N = chess.length;
+
+		for (int r = 0; r < rQueen; r++) {
+			// 세로(\)
+			if (chess[r][cQueen]) return false;
+			// 대각선(\) : r + cDaegak1(어딘지 아직 모르는 \ 대각선 상의 c) = rQueen + cQueen
+			int cDaegak1 = (rQueen + cQueen) - r;
+			if (cDaegak1 >= 0 && cDaegak1 < N && chess[r][cDaegak1]) return false;
+			// 대각선(/) : r - cDaegak1(어딘지 아직 모르는 / 대각선 상의 c) = rQueen - cQueen
+			int cDaegak2 = r - (rQueen - cQueen);
+			if (cDaegak2 >= 0 && cDaegak2 < N && chess[r][cDaegak2]) return false;
+		}
+		return true;
+	}
+
 }
--- practice8/src_coding_test/baekjoon_failed/G4_9663_N_Queen/Main.java
+++ practice8/src_coding_test/baekjoon_failed/G4_9663_N_Queen/Main.java
@@ -1,72 +1,57 @@
 package baekjoon_failed.G4_9663_N_Queen;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	
-    	int N = sc.nextInt();
+
+	static int cnt = 0;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
     	// 체스판: false / 퀸: true
     	boolean[][] chess = new boolean[N][N];
     	
-    	// chess[0][] 에서 시작 ~ 백트래킹 = 모든 경우의 수마다 가지를 친다(?) 느낌
+    	// chess[0][] 시작
     	setQueen(chess, 0);
     	System.out.println(cnt);
-    		
-    }
-    
-    static int cnt = 0;
-    
-    // rNextQueen(rQueen + 1) 에서 cNextQueensFinder 메서드로 cNextQueens 탐색
-    // chess[rQueen][cQueen] 을 true로 설정
-    // cNextQueens 에서 반복하면서 cnt++
-    static void setQueen(boolean[][] chess, int rQueen) {
-    	
-    	// rQueen + 1 -> N
-    	int N = chess.length;
-    	
-    	if (rQueen == N) {
-    		cnt++;
-    		return;
-    	}
-    	
-    	List<Integer> cNextQueens = cNextQueensFinder(chess, rQueen);
+	}
+	
+	static void setQueen(boolean[][] chess, int rQueen) {
+		
+		int N = chess.length;
+		
+		// rQueen + 1 -> N - 1 (x)
+		if (rQueen == N) {
+			cnt++;
+			return;
+		}
+		
+		for (int cQueen = 0; cQueen < N; cQueen++) {
+			if (isSafe(chess, rQueen, cQueen)) {
+				chess[rQueen][cQueen] = true;	// 퀸 놓기
+				setQueen(chess, rQueen + 1);	// 다음 행
+				chess[rQueen][cQueen] = false;	// 퀸 제거
+			}
+		}
+	}
+	
+	static boolean isSafe(boolean[][] chess, int rQueen, int cQueen) {
+		
+		int N = chess.length;
 
-    	for (int cQueen : cNextQueens) {
-    		chess[rQueen][cQueen] = true;
-    		setQueen(chess, rQueen + 1);
-    		chess[rQueen][cQueen] = false;
-    	}
-    	
-    }
-    
-    // rNextQueen 에서 놓일수 있는 cNextQueens 반환 메서드
-    static List<Integer> cNextQueensFinder(boolean[][] chess, int rNextQueen) {
+		for (int r = 0; r < rQueen; r++) {
+			// 세로(\)
+			if (chess[r][cQueen]) return false;
+			// 대각선(\) : r + cDaegak1(어딘지 아직 모르는 \ 대각선 상의 c) = rQueen + cQueen
+			int cDaegak1 = (rQueen + cQueen) - r;
+			if (cDaegak1 >= 0 && cDaegak1 < N && chess[r][cDaegak1]) return false;
+			// 대각선(/) : r - cDaegak1(어딘지 아직 모르는 / 대각선 상의 c) = rQueen - cQueen
+			int cDaegak2 = r - (rQueen - cQueen);
+			if (cDaegak2 >= 0 && cDaegak2 < N && chess[r][cDaegak2]) return false;
+		}
+		return true;
+	}
 
-    	int N = chess.length;
-    	List<Integer> cNextQueens = new ArrayList<>();
-    	
-    	outer:
-    	for (int c = 0; c < N; c++) {				// cNextQueens 후보들
-    		for (int r = 0; r < rNextQueen; r++) {	// 0 ~ rNextQueen 에서 탐색
-    			// 가로 : rNextQueen에서 cNextQueen을 찾는거라 Pass
-    			// 세로
-    			if (chess[r][c]) continue outer;
-    			// 대각선(\) : r + cDaegak1(어딘지 모르는 대각선 상의 c) = rNextQueen + c(=cNextQueen)
-    			// 대각선(/) : r - cDaegak2(어딘지 모르는 대각선 상의 c) = rNextQueen - c(=cNextQueen)
-    			int cDaegak1 = (rNextQueen + c) - r;
-    			int cDaegak2 = r - (rNextQueen - c);
-    			if (cDaegak1 >= 0 && cDaegak1 < N && chess[r][cDaegak1]) continue outer;
-    			if (cDaegak2 >= 0 && cDaegak2 < N && chess[r][cDaegak2]) continue outer;
-    		}
-    		cNextQueens.add(c);
-    	}
-    	
-    	return cNextQueens;
-    	
-    }
-    
 }
