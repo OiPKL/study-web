1f6aa129ecafc84b731af9e5128cf08a2ff4bec4
오완진 <dhkswls1998@naver.com>
2024-08-13 18:40:19 +0900

    240813

--- practice8/src_coding_test/baekjoon/Main.java
+++ practice8/src_coding_test/baekjoon/Main.java
@@ -5,33 +5,35 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	
-    	int N = sc.nextInt();
-    	// bigNum-1 : 0 ~ 9 올 수 있도록
-    	System.out.println(whatsOnNext(10, N));
-    		
-    }
-    
-    static int cnt = 0;
-    static List<Integer> num = new ArrayList<>();
-    
-    static int whatsOnNext (int bigNum, int N) {
-
-    	if (cnt == N) {
-    		int jaritsu = 1;
-    		int answer = 0;
-    		for (int i = num.size() - 1; i >= 0; i--) {
-    			answer += num.get(i) * jaritsu;
-    			jaritsu *= 10;
-    		}
-    	}
-
-    	for (int i = 0; i < bigNum; i++) {
-    		
-    	}
-    	
-    }
-    
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		decreasingNums.add(0);					// 첫째자리 0 * digit = 0 해결
+		getDecreasingNums(0, 10, 1, 1, N);		// preDigit = 10 : nextDigit = 0 ~ 9
+		if (cnt >= N) 	System.out.println(decreasingNums.get(N-1));
+		else 			System.out.println(-1);
+		
+	}
+	
+	static int cnt = 1;
+	static List<Integer> decreasingNums = new ArrayList<>();
+											// 앞자리수		현재자리수			전체자리수
+	static void getDecreasingNums(int num, int preDigit, int currDigit, int totalDigit, int N) {
+		
+		for (int digit = 0; digit < preDigit; digit++) {
+			if (num == 0 && digit == 0) continue;
+			num = num*10 + digit;
+			decreasingNums.add(num);
+			cnt++;
+			if (cnt >= N) return;
+			getDecreasingNums(num, digit, ++currDigit, totalDigit, N);
+			num /= 10;
+		}
+		
+		getDecreasingNums(0, 10, 1, ++totalDigit, N);
+		
+	}
+	
 }
--- practice8/src_coding_test/swea/Solution.java
+++ practice8/src_coding_test/swea/Solution.java
@@ -1,18 +1,52 @@
 package swea;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int TC = sc.nextInt();
 
-        for (int tc = 1; tc <= TC; tc++) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int[][] test = new int[11][10];
+    	int[] score = new int[11];
+    	
+    	for (int i = 0; i < 11; i++) {
+    		for (int j = 0; j < 10; j++) {
+    			test[i][j] = sc.nextInt();
+    		}
+    	}
+    	
+    	for (int i = 1; i <= 10; i++) {
+    		for (int j = 0; j < 10; j++) {
+    			if (test[i][j] == test[0][j])
+    				score[i] += 10;
+    		}
+    	}
+    	
+    	int maxScore = -1;
+    	int maxIdx = 0;
+    	int minScore = 101;
+    	int minIdx = 0;
 
-            System.out.println("#" + tc + " " + );
-        }
-        sc.close();
+    	for (int i = 1; i <= 10; i++) {
+    		if (maxScore < score[i]) {
+    			maxScore = score[i];
+    			maxIdx = i;
+    		}
+    		if (minScore > score[i]) {
+    			minScore = score[i];
+    			minIdx = i;
+    		}
+    	}
+    	
+    	int sum = 0;
+    	for (int i = 1; i <= 10; i++) {
+    		if (i == maxIdx || i == minIdx) continue;
+    		sum += score[i];
+    	}
+    	
+    	int answer = 0;
+    	answer = (sum+4) / 8;
+    	System.out.println(answer);
     }
 }
