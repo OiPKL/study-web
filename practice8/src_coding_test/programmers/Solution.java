46a499c288c5dfe197435d103564adee384afc94
오완진 <dhkswls1998@naver.com>
2024-08-14 18:33:27 +0900

    240814

--- practice8/src_coding_test/programmers/Solution.java
+++ practice8/src_coding_test/programmers/Solution.java
@@ -1,8 +1,25 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
 
+    	// Queue bridge 사이즈는 2 * bridge_length
+    	// Queue bridge 총합 <= weight
+    	Queue<Integer> bridge = new LinkedList<>();
+    	List<Integer> bridgeGunnu = new ArrayList<>();
+    	int time = 0;
+    	
+    	while (bridgeGunnu.size() == truck_weights.length * 2) {
+    		
+    	}
+    	
+    	return time;
+    	
     }
 }
 
--- practice8/src_coding_test/swea/Solution.java
+++ practice8/src_coding_test/swea/Solution.java
@@ -1,52 +1,54 @@
 package swea;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) {
-
-    	Scanner sc = new Scanner(System.in);
-    	
-    	int[][] test = new int[11][10];
-    	int[] score = new int[11];
-    	
-    	for (int i = 0; i < 11; i++) {
-    		for (int j = 0; j < 10; j++) {
-    			test[i][j] = sc.nextInt();
-    		}
-    	}
-    	
-    	for (int i = 1; i <= 10; i++) {
-    		for (int j = 0; j < 10; j++) {
-    			if (test[i][j] == test[0][j])
-    				score[i] += 10;
-    		}
-    	}
-    	
-    	int maxScore = -1;
-    	int maxIdx = 0;
-    	int minScore = 101;
-    	int minIdx = 0;
-
-    	for (int i = 1; i <= 10; i++) {
-    		if (maxScore < score[i]) {
-    			maxScore = score[i];
-    			maxIdx = i;
-    		}
-    		if (minScore > score[i]) {
-    			minScore = score[i];
-    			minIdx = i;
-    		}
-    	}
-    	
-    	int sum = 0;
-    	for (int i = 1; i <= 10; i++) {
-    		if (i == maxIdx || i == minIdx) continue;
-    		sum += score[i];
-    	}
-    	
-    	int answer = 0;
-    	answer = (sum+4) / 8;
-    	System.out.println(answer);
-    }
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		List<Integer> tower = new ArrayList<>();
+		List<Integer> tower1 = new ArrayList<>();
+		int sum = 0;
+		for (int n = 0; n < N; n++) {
+			int block = sc.nextInt();
+			tower.add(block);
+			sum += block;
+		}
+		
+		for (int n = 1; n <= N; n++) {
+			makeTower1(tower, tower1, 0, n, sum, 0);
+		}
+		
+		System.out.println(minSum);
+		System.out.println(Math.max(sum - memoSum, memoSum));
+		
+	}
+	
+	static int minSum = 999999999;
+	static int memoSum = 0;
+	
+	static void makeTower1(List<Integer> tower, List<Integer> tower1, int blocks, int maxBlocks, int sum, int sum1) {
+		if (blocks == maxBlocks) {
+			System.out.print("tower1: ");		//
+			for (int num : tower1) {
+				System.out.print(num + " ");	//
+				sum1 += num;
+			}									//
+			System.out.println();				//
+			minSum = Math.min(minSum, Math.abs((sum - sum1) - sum1));
+			memoSum = sum1;
+			return;
+		}
+		
+		for (int n = blocks; n < maxBlocks; n++) {
+			tower1.add(tower.get(n));
+			makeTower1(tower, tower1, blocks + 1, maxBlocks, sum, sum1);
+			tower1.remove(tower1.size()-1);
+		}
+	}
+	
 }
