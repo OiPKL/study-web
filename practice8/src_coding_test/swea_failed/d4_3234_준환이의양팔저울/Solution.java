496e0351cff2015ccaf8cd4bd67e060228153392
오완진 <dhkswls1998@naver.com>
2024-08-19 18:45:36 +0900

    240819

--- practice8/src_coding_test/swea_failed/d4_3234_준환이의양팔저울/Solution.java
+++ practice8/src_coding_test/swea_failed/d4_3234_준환이의양팔저울/Solution.java
@@ -99,3 +99,57 @@ public class Solution {
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int TC = sc.nextInt();
+        
+        for (int tc = 1; tc <= TC; tc++) {
+            int N = sc.nextInt();
+            int[] chu = new int[N];
+            for (int n = 0; n < N; n++) {
+                chu[n] = sc.nextInt();
+            }
+            cnt = 0;
+            // 초기 호출
+            setChu(chu, 0, 0, 0, N, new boolean[N]);
+            System.out.println("#" + tc + " " + cnt);
+        }
+    }
+    
+    static int cnt = 0;
+    
+    // 재귀적으로 추를 양팔저울에 배치
+    static void setChu(int[] chu, int index, int leftSum, int rightSum, int N, boolean[] used) {
+        if (index == N) {
+            cnt++;
+            return;
+        }
+        
+        for (int i = 0; i < N; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            
+            // 현재 추를 왼쪽에 놓는 경우
+            setChu(chu, index + 1, leftSum + chu[i], rightSum, N, used);
+            
+            // 현재 추를 오른쪽에 놓는 경우 (조건 확인)
+            if (leftSum >= rightSum + chu[i]) {
+                setChu(chu, index + 1, leftSum, rightSum + chu[i], N, used);
+            }
+            
+            used[i] = false;
+        }
+    }
+}
