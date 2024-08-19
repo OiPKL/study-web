1e5de0dc8aec3b26d47dba3d12590f9742a77fd1
dhkswls1998 <dhkswls1998@naver.com>
2024-08-19 22:15:51 +0900

    240819

--- practice8/src_coding_test/swea/Solution.java
+++ practice8/src_coding_test/swea/Solution.java
@@ -1,30 +1,4 @@
 package swea;
 
-import java.util.Scanner;
-
 public class Solution {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int TC = sc.nextInt();
-		for (int tc = 1; tc <= TC; tc++) {
-			
-			int cnt = 0;
-			
-			int D = sc.nextInt();
-			int W = sc.nextInt();
-			int K = sc.nextInt();
-			int[][] yak = new int[D][W];
-			for (int d = 0; d < D; d++) {
-				for (int w = 0; w < W; w++) {
-					yak[d][w] = sc.nextInt();
-				}
-			}
-			
-			System.out.println("#" + tc + " " + cnt);
-		}
-		
-	}
-	
 }
