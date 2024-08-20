106abdae630f210e47159504a7853d0a04c97d29
오완진 <dhkswls1998@naver.com>
2024-08-20 18:40:06 +0900

    240820

--- practice8/src/practice/Solution.java
+++ practice8/src/practice/Solution.java
@@ -1,70 +1,51 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner (System.in);
 		
 		int TC = sc.nextInt();
 		for (int tc = 1; tc <= TC; tc++) {
 			
 			int N = sc.nextInt();
-			int[] chu = new int[N];
-			int[] chuL = new int[N];
-			int[] chuR = new int[N];
-			sum = 0;
-			sumL = 0;
-			sumR = 0;
-			for (int n = 0; n < N; n++) {
-				int num = sc.nextInt();
-				chu[n] = num;
-				sum += num;
-			}
-			
-			cnt = 0;
-			setChu(chu, chuL, chuR);
-			
-			System.out.println("#" + tc + " " + cnt);
-		}
+			int[][] SNJ = new int[N][N];
+			for (int n = 0; n < N; n++)
+				for (int m = 0; m < N; m++)
+					SNJ[n][m] = sc.nextInt();
+
+		int great = 0;
+		simulation = new ArrayList<>();
+		combination(new int[N], new boolean[N], 0, N, N/2, new ArrayList<>());
 		
+		System.out.println(simulation.size());
+		
+		System.out.println("#" + tc + " " + great);
+		}
 	}
 	
-	static int cnt = 0;
-	static int sum = 0;
-	static int sumL = 0;
-	static int sumR = 0;
+	static List<int[]> simulation;
 	
-	static void setChu(int[] chu, int[] chuL, int[] chuR) {
-		if (sumL < sumR) return;
-		if (sum == sumL + sumR) {
-			cnt++;
-			return;
-		}
-		
-		setChu:
-		for (int n = 0; n < chu.length; n++) {
-			for (int m = 0; m < chuL.length; m++)
-				if (chu[m] == chuL[m]) continue setChu;
+	static void combination(int[] arr, boolean[] used, int start, int n, int r, List<Integer> food) {
+	    if(r == 0) {
+			for (int num : food)
+				System.out.print(num + " ");
+			System.out.println();
 			
-			chuL[n] = chu[n];
-			sumL += chu[n];
-			if (chuR[n] == chu[n]) {
-				chuR[n] = 0;
-				sumR -= chu[n];
-			}
-			
-			setChu(chu, chuL, chuR);
-			
-			chuR[n] = chu[n];
-			sumR += chu[n];
-			if (chuL[n] == chu[n]) {
-				chuL[n] = 0;
-				sumL -= chu[n];
-			}
-		}
-		
+	        return;
+	    } 
+
+	    for(int i = start; i < n; i++) {
+	        used[i] = true;
+	        food.add(i);
+	        combination(arr, used, i + 1, n, r - 1, food);
+	        used[i] = false;
+	        food.remove(food.size() - 1);
+	    }
 	}
 	
 }
--- practice8/src_coding_test/swea/Solution.java
+++ practice8/src_coding_test/swea/Solution.java
@@ -14,38 +14,68 @@ public class Solution {
 			
 			int N = sc.nextInt();
 			int[][] SNJ = new int[N][N];
-			for (int n = 0; n < N; n++) {
-				for (int m = 0; m < N; m++) {
+			for (int n = 0; n < N; n++)
+				for (int m = 0; m < N; m++)
 					SNJ[n][m] = sc.nextInt();
+
+		int great = 9999;
+		combinations = new ArrayList<>();
+		combination(N, 0, new ArrayList<>());
+		
+		// (N combination N/2) 만큼 반복
+		for (int s = 0; s < combinations.size(); s++) {
+			
+			int[] combi = combinations.get(s);
+			boolean[] food = new boolean[N];
+			for (int n = 0; n < N/2; n++)
+				food[combi[n]] = true;
+			
+			List<Integer> food1 = new ArrayList<>();
+			List<Integer> food2 = new ArrayList<>();
+			
+			for (int n = 0; n < N; n++) {
+				if (food[n]) food1.add(n);
+				else		 food2.add(n);
+			}
+			
+			int sum1 = 0;
+			int sum2 = 0;
+			int compare = 0;
+			
+			for (int i = 0; i < N/2; i++) {
+				for (int j = i+1; j < N/2; j++) {
+					sum1 += (SNJ[food1.get(i)][food1.get(j)] + SNJ[food1.get(j)][food1.get(i)]);
+					sum2 += (SNJ[food2.get(i)][food2.get(j)] + SNJ[food2.get(j)][food2.get(i)]);
 				}
 			}
 			
-			simulation = new ArrayList<>();
-			int great = Integer.MAX_VALUE;
-			combination(N, new boolean[N], 0, new ArrayList<>());
+			compare = Math.abs(sum1 - sum2);
+			great = Math.min(great, compare);
 			
-			System.out.println("#" + tc + " " + great);
+		}
+		
+		System.out.println("#" + tc + " " + great);
 		}
 	}
 	
-	static List<List<Integer>> simulation;
+	static List<int[]> combinations;
 	
-	static void combination(int N, boolean[] used, int start, List<Integer> food) {
-		if (food.size() == N/2) {
-			simulation.add(food);
-			for (int num : food)
-				System.out.print(num + " ");
-			System.out.println();
-			return;
-		}	
+	static void combination(int N, int start, List<Integer> food) {
+	    if(food.size() == N/2) {
+    	int[] foods = new int[food.size()];
+    	for (int f = 0; f < food.size(); f++)
+    		foods[f] = food.get(f);
+    	
+		combinations.add(foods);
 		
-		for (int n = start; n < N; n++) {
-			if (used[n]) continue;
-			used[n] = true;
-			combination(N, used, start + 1, food);
-			used[n] = false;
-		}
-		
+        return;
+	    }
+	    
+	    for (int i = start; i < N; i++) {
+	    	food.add(i);
+	    	combination(N, i + 1, food);
+	    	food.remove(food.size() - 1);
+	    }
 	}
 	
 }
