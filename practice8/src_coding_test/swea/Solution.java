e996e8848bf83be998931ee2b1a7aefbb7ebce24
오완진 <dhkswls1998@naver.com>
2024-08-20 10:56:16 +0900

    240820

--- practice8/src_coding_test/swea/Solution.java
+++ practice8/src_coding_test/swea/Solution.java
@@ -23,6 +23,12 @@ public class Solution {
 			// SNJ[1][3] + SNJ[3][1] .. 미리 더한 뒤에
 			// 4C2 = 6 -> 3개 합 / 3개 합 의 차이가 작은 경우가 great
 			
+			for (int n = 0; n < N; n++) {
+				for (int m = 0; m < N; m++) {
+					if (n == m) 
+				}
+			}
+			
 			System.out.println("#" + tc + " " + great);
 		}
 	}
