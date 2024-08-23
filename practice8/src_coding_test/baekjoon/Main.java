bc9747741960e1ffa0d5b37a9a0154069b6c8d68
오완진 <dhkswls1998@naver.com>
2024-08-23 10:07:09 +0900

    240823

--- practice8/src_coding_test/baekjoon/Main.java
+++ practice8/src_coding_test/baekjoon/Main.java
@@ -1,38 +1,63 @@
 package baekjoon;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	
+	static int N;
+	static int M;
+	static int minCnt;
+
+	// 하 우 상 좌
+	static int[] dn = {1, 0, -1, 0};
+	static int[] dm = {0, 1, 0, -1};
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		N = sc.nextInt();
+		M = sc.nextInt();
+		sc.nextLine();
 		
-		List<Integer> printN = new ArrayList<>();
-		pickN(N, M, printN);
+		// (0,0) 출발 ~ (N,M) 도착
+		int[][] miro = new int[N][M];
+		for (int n = 0; n < N; n++) {
+			String str = sc.nextLine();
+			char[] charArr = str.toCharArray();
+			for (int m = 0; m < M; m++) {
+				miro[n][m] = charArr[m] - '0';
+			}
+		}
+		
+		minCnt = 9999;
+		move(miro, 0, 0, 0, 1);
+		
+		System.out.println(minCnt);
 		
 	}
 	
-	static void pickN(int N, int M, List<Integer> printN) {
-		if (printN.size() == M) {
-			for (int num : printN)
-				System.out.print(num + " ");
-			System.out.println();
+	// dWay -> Now 에서 어느 방향에서 왔는지 체크 -> 온 방향으로 되돌아가지 않도록
+	static void move(int[][] miro, int nNext, int mNext, int dWay, int cnt) {
+		
+		if (miro[nNext][mNext] == 0) return;
+		
+		// TEST -----------------------------------------------------------
+		System.out.println("[" + nNext + ", " + mNext + "]");
+		// TEST -----------------------------------------------------------
+		
+		if (nNext == N-1 && mNext == M-1) {
+			minCnt = Math.min(minCnt, cnt);
 			return;
 		}
 		
-		pickN:
-		for (int n = 1; n <= N; n++) {
-			// N과 M(2) 와 달리 1부터 다시 돌릴껀데 만약 중복되는 수가 있다면 Pass
-			for (int m = 0; m < printN.size(); m++)
-				if (n == printN.get(m)) continue pickN;
-			printN.add(n);
-			pickN(N, M, printN);
-			printN.remove(printN.size() - 1);
+		for (int d = 0; d < 4; d++) {
+			if (d == (dWay+2) % 4 ) continue;	// 온 방향 Pass
+			if (nNext+dn[d] < 0 || nNext+dn[d] == N ||
+				mNext+dm[d] < 0 || mNext+dm[d] == M) continue;
+			move(miro, nNext+dn[d], mNext+dm[d], d, cnt + 1);
 		}
+		
 	}
+	
 }
