df40a4f108f0ea0b84d03d82896d96822042f18e
dhkswls1998 <dhkswls1998@naver.com>
2024-08-24 16:57:49 +0900

    240824

--- practice8/src_coding_test/baekjoon/Main.java
+++ practice8/src_coding_test/baekjoon/Main.java
@@ -1,79 +1,90 @@
 package baekjoon;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	
-	static List<List<Integer>> pickedList = new ArrayList<>();
-	
+	static int[] dn = new int[] {1, 0, -1, 0};	// 하 우 상 좌
+	static int[] dm = new int[] {0, 1, 0, -1};
+//	static int[] dn = new int[] {0, 1, 0, -1};	// 우 하 좌 상
+//	static int[] dm = new int[] {1, 0, -1, 0};
+
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
 		int M = sc.nextInt();
-		List<int[]> houseList = new ArrayList<>();
-		List<int[]> chickList = new ArrayList<>();
-		for (int n1 = 0; n1 < N; n1++) {
-			for (int n2 = 0; n2 < N; n2++) {
-				int num = sc.nextInt();
-				if (num == 1) houseList.add(new int[] {n1, n2});
-				if (num == 2) chickList.add(new int[] {n1, n2});
+		sc.nextLine();
+		
+		int[][] miro = new int[N][M];
+		boolean[][] visited = new boolean[N][M];
+//		Stack<int[]> stack = new Stack<>();
+        Queue<int[]> queue = new LinkedList<>();
+		
+		for (int n = 0; n < N; n++) {
+			String str = sc.nextLine();
+			char[] charArr = str.toCharArray();
+			for (int m = 0; m < M; m++) {
+				miro[n][m] = charArr[m] - '0';
 			}
 		}
 		
-		// [집No][집No-치킨No 치킨거리]
-		// 집1 - {치킨거리A, 치킨거리B, ...}
-		// 집2 - {치킨거리A, 치킨거리B, ...}
-		// 치킨 A,B,C ... 중 M개만 남겨서 집[1][min]+집[2][min]+... 최소 구하기
-		int H = houseList.size();
-		int C = chickList.size();
-		int[][] chickD = new int[H][C];
-		for (int h = 0; h < H; h++) {
-			for (int c = 0; c < C; c++) {
-				int[] house = houseList.get(h);
-				int[] chick = chickList.get(c);
-				chickD[h][c] = getD(house[0], house[1], chick[0], chick[1]);
+		// (0,0) 시작 -> (N-1,M-1) 도착
+//		stack.push(new int[]{0, 0, 1});	// 좌표+cnt
+        queue.add(new int[] {0, 0, 1}); // 좌표+cnt
+		visited[0][0] = true;
+		
+		int minCnt = Integer.MAX_VALUE;
+		
+//		while(!stack.isEmpty()) {
+//			int[] now = stack.pop();
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+			int nNow = now[0];
+			int mNow = now[1];
+			int cnt = now[2];
+			
+			if (nNow == N - 1 && mNow == M - 1) {
+				minCnt = Math.min(minCnt, cnt);
 			}
+			
+			List<int[]> next = searchWay(miro, visited, now);
+			
+            for (int[] nxt : next) {
+                int nNext = nxt[0];
+                int mNext = nxt[1];
+                visited[nNext][mNext] = true;
+//                stack.push(new int[]{nNext, mNext, cnt + 1});
+                queue.add(new int[]{nNext, mNext, cnt + 1});
+            }
 		}
 		
-		combi(C, M, 0, new ArrayList<>());
-		
-		int minChickD = Integer.MAX_VALUE;
-		for (List<Integer> picked : pickedList) {
-			int minChiSum = 0;
-			for (int h = 0; h < H; h++) {
-				int minChiDH = Integer.MAX_VALUE;
-				for (int p : picked) {
-					minChiDH = Math.min(minChiDH, chickD[h][p]);
-				}
-				minChiSum += minChiDH;
-			}
-			minChickD = Math.min(minChickD, minChiSum);
-		}
-		
-		System.out.println(minChickD);
+		System.out.println(minCnt);
 		
 	}
 	
-	static int getD(int r1, int c1, int r2, int c2) {
-		int distance = Math.abs(r1 - r2) + Math.abs(c1 - c2); 
-		return distance;
-	}
-
-	static void combi(int C, int M, int idx, List<Integer> picked) {
-		if (M == picked.size()) {
-			pickedList.add(new ArrayList<>(picked));
-			return;
+	static List<int[]> searchWay(int[][] miro, boolean[][] visited, int[] now) {
+		int N = miro.length;
+		int M = miro[0].length;
+		
+		List<int[]> next = new ArrayList<>();
+		for (int d = 0; d < 4; d++) {
+			int nNext = now[0] + dn[d];
+			int mNext = now[1] + dm[d];
+			
+			if (nNext < 0 || nNext == N) continue;
+			if (mNext < 0 || mNext == M) continue;
+			if (miro[nNext][mNext] == 0) continue;
+			if (visited[nNext][mNext]) continue;
+			
+			next.add(new int[] {nNext, mNext});
 		}
 		
-		for (int c = idx; c < C; c++) {
-			picked.add(c);
-			combi(C, M, c + 1, picked);
-			picked.remove(picked.size() - 1);
-		}
+		return next;
 	}
 	
 }
