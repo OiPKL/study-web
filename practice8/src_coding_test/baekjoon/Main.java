00701cce1b08847fca91a840489fe53a0eff51a3
오완진 <dhkswls1998@naver.com>
2024-08-27 18:45:38 +0900

    240827

--- practice8/src_coding_test/baekjoon/Main.java
+++ practice8/src_coding_test/baekjoon/Main.java
@@ -1,5 +1,33 @@
 package baekjoon;
 
-public class Main {
+import java.util.Scanner;
+import java.util.Stack;
 
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// 1. 맨 뒤 num 추가
+		// 2. 맨 뒤 원소 제거
+		// >> 맨 뒤에서부터 최소 몇 개를 선택해야 mod로 나누었을 때
+		//    0, 1, ... mod-1 경우가 최소 한 번 이상 ??
+		
+		int Q = sc.nextInt();	// Query 개수
+		int M = sc.nextInt();	// mod
+		
+		Stack[] stackList = new Stack[M];
+		
+		int idx = 0;
+		for (int q = 0; q < Q; q++) {
+			int cmd = sc.nextInt();
+			
+			if (cmd == 1) {
+				long num = sc.nextLong();
+				stackList[(int) num % M].add(idx++);
+			}
+		}
+		
+	}
 }
