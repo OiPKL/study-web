3b259cb0864cba13bd88ff1e3fcbf4863c707c62
오완진 <dhkswls1998@naver.com>
2024-08-28 18:02:28 +0900

    240828

--- practice8/src_coding_test/baekjoon/Main.java
+++ practice8/src_coding_test/baekjoon/Main.java
@@ -1,45 +1,7 @@
 package baekjoon;
 
-import java.util.Scanner;
-import java.util.Stack;
-
 public class Main {
-	
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		// 1. 맨 뒤 num 추가
-		// 2. 맨 뒤 원소 제거
-		// >> 맨 뒤에서부터 최소 몇 개를 선택해야 mod로 나누었을 때
-		//    0, 1, ... mod-1 경우가 최소 한 번 이상 ??
-		
-		int Q = sc.nextInt();	// Query 개수
-		int M = sc.nextInt();	// mod
-		
-		Stack[] mods = new Stack[M];
-		Stack lastAdded = new Stack<>();
-		
-		int mod;
-		long num;
-		int idx = 0;
-		for (int q = 0; q < Q; q++) {
-			int cmd = sc.nextInt();
-			
-			if (cmd == 1) {
-				num = sc.nextLong();
-				mod = (int) num % M;
-				mods[mod].add(idx);
-				lastAdded.add(idx++);
-			} else if (cmd == 2) {
-				if (!lastAdded.isEmpty()) {
-					mod = (int) lastAdded.pop();
-					mods[mod].pop();
-				}
-			} else {
-				
-			}
-		}
 		
 	}
 }
--- practice8/src_coding_test/swea/Solution.java
+++ practice8/src_coding_test/swea/Solution.java
@@ -1,83 +1,38 @@
 package swea;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
+	
 	static int N;
-	static int[][] SJR;
-	static List<List<Integer>> foodsList;
+	static int M;
+	static int num;
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int TC = sc.nextInt();
-		for (int tc = 1; tc <= TC; tc++) {
+		for (int tc = 1; tc <= 10; tc++) {
 			
+			int TC = sc.nextInt();
 			N = sc.nextInt();
-			SJR = new int[N][N];
-			foodsList = new ArrayList<>();
-			for (int n1 = 0; n1 < N; n1++)
-				for (int n2 = 0; n2 < N; n2++)
-					SJR[n1][n2] = sc.nextInt();
+			M = sc.nextInt();
+			num = 1;
 			
-			combination(0, new ArrayList<>());
+			zgop(false, 0);
 			
-			int minGap = Integer.MAX_VALUE;
-			for (int fl = 0; fl < foodsList.size(); fl++) {
-				List<Integer> foods = foodsList.get(fl);
-				List<Integer> foodA = new ArrayList<>();
-				List<Integer> foodB = new ArrayList<>();
-				
-				boolean[] isFoodA = new boolean[N];
-				for (int n = 0; n < N/2; n++)
-					isFoodA[foods.get(n)] = true;
-				
-				for (int n = 0; n < N; n++) {
-					if (isFoodA[n])	foodA.add(n);
-					else			foodB.add(n);
-				}
-				
-				int scoreA = 0;
-				for (int a1 = 0; a1 < N/2 - 1; a1++) {
-					for (int a2 = a1 + 1; a2 < N/2; a2++) {
-						int idx1 = foodA.get(a1);
-						int idx2 = foodA.get(a2);
-						scoreA += (SJR[idx1][idx2] + SJR[idx2][idx1]);
-					}
-				}
-				int scoreB = 0;
-				for (int b1 = 0; b1 < N/2 - 1; b1++) {
-					for (int b2 = b1 + 1; b2 < N/2; b2++) {
-						int idx1 = foodB.get(b1);
-						int idx2 = foodB.get(b2);
-						scoreB += (SJR[idx1][idx2] + SJR[idx2][idx1]);
-					}
-				}
-				int gap = Math.abs(scoreA - scoreB);
-				minGap = Math.min(minGap, gap);
-						
-			}
-			
-			System.out.println("#" + tc + " " + minGap);
+			System.out.println("#" + TC + " " + num);
 		
 		}
 		
 	}
 	
-	static void combination(int idx, List<Integer> foods) {
-		if (foods.size() == N/2) {
-			foodsList.add(new ArrayList<>(foods));
-			return;
-		}
-		
-		for (int i = idx; i < N; i++) {
-			foods.add(i);
-			combination(i + 1, foods);
-			foods.remove(foods.size() - 1);
-		}
+	static void zgop(boolean isComplete, int cnt) {
+		if (isComplete) return;
+		if (cnt == M - 1) isComplete = true;
+
+		num *= N;
+		zgop(isComplete, cnt + 1);
 	}
 	
 }
