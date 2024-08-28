35fd7f3f81a04bea19f1ab271c4a399ef5a51ab7
오완진 <dhkswls1998@naver.com>
2024-08-28 18:45:18 +0900

    240828

--- practice8/src_coding_test/baekjoon/Main.java
+++ practice8/src_coding_test/baekjoon/Main.java
@@ -1,7 +1,19 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		/*
+		 * 1. X : 0 _ _
+		 * 2. X 0,1 <-> X' 1,0
+		 * 3. X += X'
+		 * ... K 번째 문자?
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int K = sc.nextInt();
 		
 	}
 }
