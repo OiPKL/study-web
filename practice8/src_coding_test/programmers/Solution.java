b0e63fe61f606314320d688533274425dd78602d
오완진 <dhkswls1998@naver.com>
2024-08-30 10:11:50 +0900

    240830

--- practice8/src_coding_test/programmers/Solution.java
+++ practice8/src_coding_test/programmers/Solution.java
@@ -1,21 +1,25 @@
 package programmers;
 
 class Solution {
-    public static int solution(int n, int a, int b) {
+    public int solution(int n, int a, int b) {
     	int cnt = 0;
     	
-    	while(Math.abs(a - b) != 1) {
-    		a /= 2;
-    		b /= 2;
+    	while(a != b) {
+    		if (a % 2 != 0) {
+    			a = (a + 1) / 2;
+    		} else {
+    			a = a / 2;
+    		}
+    		
+    		if (b % 2 != 0) {
+    			b = (b + 1) / 2;
+    		} else {
+    			b = b / 2;
+    		}
+
     		cnt++;
     	}
     	
     	return cnt;
     }
-}
-
-class Main {
-	public static void main(String[] args) {
-		System.out.println(Solution.solution(8, 4, 7));
-	}
 }
