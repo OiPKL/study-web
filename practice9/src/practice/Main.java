85407609664b3a6598524fb04b2530a364ad837c
오완진 <dhkswls1998@naver.com>
2024-09-02 18:45:57 +0900

    240902

--- practice9/src/practice/Main.java
+++ practice9/src/practice/Main.java
@@ -1,9 +1,23 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	static int N;
+	static int[] NN;
+	
     public static void main(String[] args) {
     	
+    	Scanner sc = new Scanner(System.in);
+
+    	N = sc.nextInt();
+    	NN = new int[N];
+    	for (int n = 0; n < N; n++)
+    		NN[n] = sc.nextInt();
     	
+    	for (int )
     	
     }
+    
 }
