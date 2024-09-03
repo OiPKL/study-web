9a252ce52618905f26b791b3f425ca3301478bba
오완진 <dhkswls1998@naver.com>
2024-09-03 18:32:09 +0900

    240903

--- practice9/src_coding_test/baekjoon/Main.java
+++ practice9/src_coding_test/baekjoon/Main.java
@@ -1,40 +1,56 @@
 package baekjoon;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	
+	static int N, rNow, cNow, rNext, cNext, dWay;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dr = {0, 1, 0, -1};
+	static int[] dc = {-1, 0, 1, 0};
+	// (4방향 모두 y기준) 2 10 7 1 5 10 7 1 2 a 순으로 저장
+	static int[][] dtr = {{-2, -1, -1, -1, 0, 1, 1, 1, 2, 0},
+						  {0, 1, 0, -1, 2, 1, 0, -1, 0, 1},
+						  {2, 1, 1, 1, 0, -1, -1, -1, -2, 0},
+						  {0, -1, 0, 1, -2, -1, 0, 1, 0, -1}};
+	static int[][] dtc = {{0, -1, 0, 1, -2, -1, 0, 1, 0, -1},
+						  {-2, -1, -1, -1, 0, 1, 1, 1, 2, 0},
+						  {0, 1, 0, -1, 2, 1, 0, -1, 0, 1},
+						  {2, 1, 1, 1, 0, -1, -1, -1, -2, 0}};
+	static int[] dtx = 	  {2, 10, 7, 1, 5, 10, 7, 1, 2};
+	
 	public static void main(String[] args) {
-
-		StringBuilder sb = new StringBuilder();
+		
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt();
-		int[] NN = new int[N];
-		boolean checked = false;
-		for (int n = 0; n < N; n++)
-			NN[n] = sc.nextInt();
+		N = sc.nextInt();
+		map = new int[N][N];
+		visited = new boolean[N][N];
+		for (int r = 0; r < N; r++)
+			for (int c = 0; c < N; c++)
+				map[r][c] = sc.nextInt();
 		
-		for (int n = N - 1; n > 0; n--) {
-			if (NN[n - 1] < NN[n]) {
-				// Swap
-				int tmp = NN[n - 1];
-				NN[n - 1] = NN[n];
-				NN[n] = tmp;
+		rNow = N/2;
+		cNow = N/2;
+		visited[rNow][cNow] = true;
+		dWay = 3;
+		
+		while (!(rNow == 0 && cNow == 0)) {
+			for (int d = 1; d <= 2; d++) {		// 좌회전 시도 후 직진
+				dWay = (dWay + d) % 4;
+				rNext = rNow + dr[dWay];
+				cNext = cNow + dc[dWay];
 				
-				Arrays.sort(NN, n, N);
-				checked = true;
-				break;
+//				if (rNext < 0 && N <= rNext) ----- 걸릴 일이 없지 ㅇㅇ
+				if (!visited[rNext][cNext]) break;
 			}
+			System.out.println(rNext + ", " + cNext + ", " + dWay);
+			rNow = rNext;
+			cNow = cNext;
+			visited[rNow][cNow] = true;
 		}
 		
-		if (checked) {
-			for (int num : NN)
-				sb.append(num).append(" ");
-			System.out.println(sb);
-		} else
-			System.out.println(-1);
 		
 	}
 }
