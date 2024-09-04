4d31d43be365c958c7e30a5fdbffb238fafb60e5
오완진 <dhkswls1998@naver.com>
2024-09-04 18:00:33 +0900

    240904

--- practice9/src_coding_test/swea/Solution.java
+++ practice9/src_coding_test/swea/Solution.java
@@ -1,73 +1,73 @@
 package swea;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
 	
-	static int N;
-	static List<List<Integer>> orderList;
+	static int[] P;
 	
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int TC = sc.nextInt();
 		for (int tc = 1; tc <= TC; tc++) {
 			
-			N = sc.nextInt();
-			int[] nums = new int[N];
-			int[] ops = new int[4];
-			for (int n = 0; n < 4; n++)
-				ops[n] = sc.nextInt();
-			for (int n = 0; n < N; n++)
-				nums[n] = sc.nextInt();
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			P = new int[N + 1];
+			int[][] edges = new int[M][2];
 			
-			long max = Long.MIN_VALUE;
-			long min = Long.MAX_VALUE;
-			
-			orderList = new ArrayList<>();
-			makeOrderList(ops, new ArrayList<>());
-			
-			for (List<Integer> order : orderList) {
-				long ans = nums[0];
-				for (int i = 1; i < N; i++) {
-					int num = nums[i];
-					int op = order.get(i - 1);
-					
-					if (op == 0)		ans += num;
-					if (op == 1)		ans -= num;
-					if (op == 2)		ans *= num;
-					if (op == 3) {
-						if (ans > 0)	ans /= num;
-						else			ans = -(-ans)/num;
-					
-					}
-				}
-				max = Math.max(max, ans);
-				min = Math.min(min, ans);
+			for (int m = 0; m < M; m++) {
+				edges[m][0] = sc.nextInt();
+				edges[m][1] = sc.nextInt();
 			}
 			
-			System.out.println("#" + tc + " " + (max - min));
+			for (int n = 1; n < N; n++) {
+				makeSet(n);
+			}
+			
+			for (int m = 0; m < M; m++) {
+				
+				int px = findSet(edges[m][0]);
+				int py = findSet(edges[m][1]);
+				
+				if (px != py) {
+					union(px, py);
+				}
+				
+				for (int num : P)
+					System.out.print(num + " ");
+				System.out.println();
+				
+			}
+			
+			int ans = 1;
+			int tmp = P[1];
+			for (int n = 2; n <= N; n++) {
+				if (P[n] != tmp) {
+					tmp = P[n];
+					ans++;
+				}
+			}
+			
+//			System.out.println("#" + tc + " " + ans);
 		}
+		
 	}
 	
-	static void makeOrderList(int[] ops, List<Integer> order) {
-		
-		if (order.size() == N - 1) {
-			orderList.add(new ArrayList<>(order));
-			return;
-		}
-		
-		for (int i = 0; i < 4; i++) {
-			if (ops[i] > 0) {
-				ops[i]--;
-				order.add(i);
-				makeOrderList(ops, order);
-				order.remove(order.size() - 1);
-				ops[i]++;
-			}
-		}
-		
+	static void makeSet (int x) {
+		P[x] = x;
 	}
+	
+	static int findSet (int x) {
+		if (x != P[x])
+			P[x] = findSet(P[x]);
+		return P[x];
+	}
+	
+	static void union(int x, int y) {
+		P[y] = x;
+	}
+	
 }
