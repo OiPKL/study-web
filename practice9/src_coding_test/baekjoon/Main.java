a3e459d770729814869a2083fa7dd34c1e22bd62
dhkswls1998 <dhkswls1998@naver.com>
2024-09-04 23:50:47 +0900

    240904

--- practice9/src_coding_test/baekjoon/Main.java
+++ practice9/src_coding_test/baekjoon/Main.java
@@ -1,92 +1,135 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	
-	static int[] dr = {0, 1, 0, -1};
-	static int[] dc = {-1, 0, 1, 0};
-	// (4방향 모두 y기준) 2 10 7 1 5 10 7 1 2 a 순으로 저장
-	static int[][] dtr = {{-2, -1, -1, -1, 0, 1, 1, 1, 2, 0},
-						  {0, 1, 0, -1, 2, 1, 0, -1, 0, 1},
-						  {2, 1, 1, 1, 0, -1, -1, -1, -2, 0},
-						  {0, -1, 0, 1, -2, -1, 0, 1, 0, -1}};
-	static int[][] dtc = {{0, -1, 0, 1, -2, -1, 0, 1, 0, -1},
-						  {-2, -1, -1, -1, 0, 1, 1, 1, 2, 0},
-						  {0, 1, 0, -1, 2, 1, 0, -1, 0, 1},
-						  {2, 1, 1, 1, 0, -1, -1, -1, -2, 0}};
-	static int[] dtp = 	  {2, 10, 7, 1, 5, 10, 7, 1, 2};
-	
+	static List<List<Integer>> pickList = new ArrayList<>();
+
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		int[][] map = new int[N][N];
-		boolean[][] visited = new boolean[N][N];
-		for (int r = 0; r < N; r++)
-			for (int c = 0; c < N; c++)
-				map[r][c] = sc.nextInt();
+		int M = sc.nextInt();
+		int[][] map = new int[N][M];
 		
-		int rNow = N/2;
-		int cNow = N/2;
-		visited[rNow][cNow] = true;
-		int dWay = 3;	// 디폴트 북쪽
-		int cnt = 0;
+		for (int n = 0; n < N; n++)
+			for (int m = 0; m < M; m++)
+				map[n][m] = sc.nextInt();
 		
-		// 토네이도 이동 : [중앙] ~ [0,0]
-		while (!(rNow == 0 && cNow == 0)) {
-			boolean goStraight = false;
-			dWay = (dWay + 1) % 4;
-			int rNext = rNow + dr[dWay];
-			int cNext = cNow + dc[dWay];
-			
-			if (visited[rNext][cNext]) goStraight = true;
-			
-			if (goStraight) {
-				dWay = (dWay + 3) % 4;
-				rNext = rNow + dr[dWay];
-				cNext = cNow + dc[dWay];
-			}
-			
-			rNow = rNext;
-			cNow = cNext;
-			visited[rNow][cNow] = true;
-			
-//			System.out.println(rNow + ", " + cNow);
+		int maxSum = Integer.MIN_VALUE;
+		
+		combination(new ArrayList<>());			// 6C2
 
-			int sand = map[rNow][cNow];
-			// 모래 이동
-			if (sand > 0) {
+		// 4 x 1
+		for (int n = 0; n < N; n++) {
+			for (int m = 0; m <= M - 4; m++) {
 				int sum = 0;
-				int rCheck;
-				int cCheck;
+				sum += map[n][m];
+				sum += map[n][m+1];
+				sum += map[n][m+2];
+				sum += map[n][m+3];
+				maxSum = Math.max(maxSum, sum);
+			}
+		}
+		// 1 x 4
+		for (int n = 0; n <= N - 4; n++) {
+			for (int m = 0; m < M; m++) {
+				int sum = 0;
+				sum += map[n][m];
+				sum += map[n+1][m];
+				sum += map[n+2][m];
+				sum += map[n+3][m];
+				maxSum = Math.max(maxSum, sum);
+			}
+		}
+		// 2 x 2
+		for (int n = 0; n <= N -2; n++) {
+			for (int m = 0; m <= M - 2; m++) {
+				int sum = 0;
+				sum += map[n][m];
+				sum += map[n][m+1];
+				sum += map[n+1][m];
+				sum += map[n+1][m+1];
+				maxSum = Math.max(maxSum, sum);
+			}
+		}
+		// 3 x 2 -> 4개 pick
+		for (int n = 0; n <= N - 2; n++) {
+			for (int m = 0; m <= M - 3; m++) {
+				int sum = 0;
+				int[] tet = {map[n][m], map[n][m+1], map[n][m+2], map[n+1][m], map[n+1][m+1], map[n+1][m+2]};
 				
-				for (int i = 0; i <= 8; i++) {
+				int minSum = Integer.MAX_VALUE;
+                for (List<Integer> pick : pickList) {
+                	
+                	if (pick.get(0) == 0 && pick.get(1) == 4) continue;
+					if (pick.get(0) == 1 && pick.get(1) == 3) continue;
+					if (pick.get(0) == 1 && pick.get(1) == 4) continue;
+					if (pick.get(0) == 1 && pick.get(1) == 5) continue;
+					if (pick.get(0) == 2 && pick.get(1) == 4) continue;
 					
-					int move = sand * dtp[i] / 100;
-					sum += move;
-					
-					rCheck = rNow + dtr[dWay][i];
-					cCheck = cNow + dtc[dWay][i];
-					
-					if (0 <= rCheck && rCheck < N && 0 <= cCheck && cCheck < N)
-						map[rCheck][cCheck] += move;
-					else
-						cnt += move;
+                    int tmp = tet[pick.get(0)] + tet[pick.get(1)];
+					minSum = Math.min(minSum, tmp);			// 2개 최소값 -> 나머지 4개 최대값
 				}
 				
-				rCheck = rNow + dtr[dWay][9];
-				cCheck = cNow + dtc[dWay][9];
+				for (int i = 0; i < 6; i++)
+					sum += tet[i];
 				
-				if (0 <= rCheck && rCheck < N && 0 <= cCheck && cCheck < N)
-					map[rCheck][cCheck] += (sand - sum);
-				else
-					cnt += (sand - sum);
+				sum -= minSum;
+				
+				maxSum = Math.max(maxSum, sum);
+			}
+		}
+		// 2 x 3 -> 4개 pick
+		for (int n = 0; n <= N - 3; n++) {
+			for (int m = 0; m <= M - 2; m++) {
+				int sum = 0;
+				int[] tet = {map[n][m], map[n][m+1], map[n+1][m], map[n+1][m+1], map[n+2][m], map[n+2][m+1]};
+				
+				int minSum = Integer.MAX_VALUE;
+                for (List<Integer> pick : pickList) {
+                	
+					if (pick.get(0) == 0 && pick.get(1) == 3) continue;
+					if (pick.get(0) == 1 && pick.get(1) == 2) continue;
+					if (pick.get(0) == 2 && pick.get(1) == 3) continue;
+					if (pick.get(0) == 2 && pick.get(1) == 5) continue;
+					if (pick.get(0) == 3 && pick.get(1) == 4) continue;
+					
+                    int tmp = tet[pick.get(0)] + tet[pick.get(1)];
+					minSum = Math.min(minSum, tmp);			// 2개 최소값 -> 나머지 4개 최대값
+				}
+				
+				for (int i = 0; i < 6; i++)
+					sum += tet[i];
+				
+				sum -= minSum;
+				
+				maxSum = Math.max(maxSum, sum);
 			}
 		}
 		
-		System.out.println(cnt);
+		System.out.println(maxSum);
+	}
+	
+	// 6 combination 2 = 15
+	static void combination(List<Integer> pick) {
+		
+		if (pick.size() == 2) {
+			pickList.add(new ArrayList<>(pick));
+			return;
+		}
+		
+		for (int i = 0; i < 6; i++) {
+			pick.add(i);
+			combination(pick);
+			pick.remove(pick.size() - 1);
+		}
 		
 	}
+	
 }
--- practice9/src_coding_test/swea/Solution.java
+++ practice9/src_coding_test/swea/Solution.java
@@ -4,70 +4,15 @@ import java.util.Scanner;
 
 public class Solution {
 	
-	static int[] P;
-	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
 		int TC = sc.nextInt();
 		for (int tc = 1; tc <= TC; tc++) {
-			
-			int N = sc.nextInt();
-			int M = sc.nextInt();
-			P = new int[N + 1];
-			int[][] edges = new int[M][2];
-			
-			for (int m = 0; m < M; m++) {
-				edges[m][0] = sc.nextInt();
-				edges[m][1] = sc.nextInt();
-			}
-			
-			for (int n = 1; n < N; n++) {
-				makeSet(n);
-			}
-			
-			for (int m = 0; m < M; m++) {
-				
-				int px = findSet(edges[m][0]);
-				int py = findSet(edges[m][1]);
-				
-				if (px != py) {
-					union(px, py);
-				}
-				
-				for (int num : P)
-					System.out.print(num + " ");
-				System.out.println();
-				
-			}
-			
-			int ans = 1;
-			int tmp = P[1];
-			for (int n = 2; n <= N; n++) {
-				if (P[n] != tmp) {
-					tmp = P[n];
-					ans++;
-				}
-			}
-			
-//			System.out.println("#" + tc + " " + ans);
-		}
+
+//            System.out.println("#" + tc + " " + );
+        }
 		
 	}
-	
-	static void makeSet (int x) {
-		P[x] = x;
-	}
-	
-	static int findSet (int x) {
-		if (x != P[x])
-			P[x] = findSet(P[x]);
-		return P[x];
-	}
-	
-	static void union(int x, int y) {
-		P[y] = x;
-	}
-	
 }
