37fe742617d0b158b8084b6b6ffb1ac7114723f0
dhkswls1998 <dhkswls1998@naver.com>
2024-09-08 20:35:35 +0900

    240908

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -1,110 +1,84 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 	
-	static int[] dw1 = {-1, 0, -1, 1, -1, 0};	// h 홀수
-	static int[] dw2 = {0, 1, -1, 1, 0, 1};		// h 짝수
-	static int[] dh  = {-1, -1, 0, 0, 1, 1};
-	
-	static int W, H, cnt;
-	static int[][] map;
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	static int R = 12, C = 6;
+	static char[][] ppuyo;
+	static int combo;
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
-		W = sc.nextInt();
-		H = sc.nextInt();
-		cnt = 0;
-		
-		map = new int[H][W];
-		
-		for (int h = 0; h < H; h++)
-			for (int w = 0; w < W; w++)
-				map[h][w] = sc.nextInt();
-		
-		for (int h = 0; h < H; h++) {
-			for (int w = 0; w < W; w++) {
-				if (map[h][w] == 1) calOuter(h, w);
-				else				calInner(h, w);
-			}
+
+		ppuyo = new char[R][C];
+		for (int r = 0; r < R; r++) {
+			char[] blocks = sc.nextLine().toCharArray();
+			for (int c = 0; c < C; c++)
+				ppuyo[r][c] = blocks[c];
 		}
 		
-		System.out.println(cnt);
-	}
-	
-	static void calOuter(int hNow, int wNow) {
-		
-		for (int d = 0; d < 6; d++) {
-			int hNext = hNow + dh[d];
-			int wNext;
-			if (hNow % 2 != 0)
-				wNext = wNow + dw1[d];
-			else
-				wNext = wNow + dw2[d];
-			
-			if (hNext < 0 || H <= hNext || wNext < 0 || W <= wNext) {
-				cnt++;
-				continue;
-			} else {
-				if (map[hNext][wNext] == 0) cnt++;
-			}
-		}
-	}
-	
-	static void calInner(int hNow, int wNow) {
-		
-		if (isHided(hNow, wNow)) {
-			
-			for (int d = 0; d < 6; d++) {
-				int hNext = hNow + dh[d];
-				int wNext;
-				if (hNow % 2 != 0)
-					wNext = wNow + dw1[d];
-				else
-					wNext = wNow + dw2[d];
+		combo = 0;
+		for (int r = 0; r < R; r++) {
+			for (int c = 0; c < C; c++) {
+				char block = ppuyo[r][c];
 				
-				if (map[hNext][wNext] == 1) cnt--;
-			}
-		}
-	}
-	
-	static boolean isHided(int hNow, int wNow) {
+				if (block != '.') {
+					
+					Queue<int[]> bfs = new LinkedList<>();
+					boolean[][] visited = new boolean[R][C];
+					List<Integer>[] boom = new ArrayList[C];
+					for (int cc = 0; cc < C; cc++)
+						boom[cc] = new ArrayList<>();
+					bfs.add(new int[] {r, c});
+					visited[r][c] = true;
+					int cnt = 0;
+					
+					while(!bfs.isEmpty()) {
+						int[] now = bfs.poll();
+						int rNow = now[0];
+						int cNow = now[1];
+						
+						boom[cNow].add(rNow);
+						cnt++;
+						
+						for (int d = 0; d < 4; d++) {
+							int rNext = rNow + dr[d];
+							int cNext = cNow + dc[d];
+							
+							if (rNext < 0 || R <= rNext) continue;
+							if (cNext < 0 || C <= cNext) continue;
+							if (ppuyo[rNext][cNext] != block) continue;
+							if (visited[rNext][cNext]) continue;
+							
+							bfs.add(new int[] {rNext, cNext});
+							visited[rNext][cNext] = true;
+						}//for d
+					}//while
+					
+					if (cnt >= 4) {
+						
+						for (int cc = 0; cc < C; cc++) {
+							if (!boom[cc].isEmpty()) {
+								Deque<Integer> dq = new LinkedList<>();
+								
+								
+							}// if isEmpty
+						}//for cc
+						
+					}// if cnt
+					
+				}//if .
+			}//for c
+		}//for r
 		
-		boolean[][] visited = new boolean[H][W];
-		Queue<int[]> bfs = new LinkedList<>();
-		visited[hNow][wNow] = true;
-		bfs.add(new int[] {hNow, wNow});
-		
-		while (!bfs.isEmpty()) {
-			
-			int[] now = bfs.poll();
-			hNow = now[0];
-			wNow = now[1];
-			
-			for (int d = 0; d < 6; d++) {
-				int hNext = hNow + dh[d];
-				int wNext;
-				if (hNow % 2 != 0)
-					wNext = wNow + dw1[d];
-				else
-					wNext = wNow + dw2[d];
-				
-				if (hNext < 0 || H <= hNext || wNext < 0 || W <= wNext)
-					return false;
-				if (visited[hNext][wNext])
-					continue;
-				if (map[hNext][wNext] == 0) {
-					visited[hNext][wNext] = true;
-					bfs.add(new int[] {hNext, wNext});
-				}
-			}
-		}
-		return true;
-	}
-	
+	}//main
 }
--- ssafy-note/coding_test/programmers/Solution.java
+++ ssafy-note/coding_test/programmers/Solution.java
@@ -1,25 +1,32 @@
 package programmers;
 
-class Solution {
-    public int solution(int n, int a, int b) {
-    	int cnt = 0;
-    	
-    	while(a != b) {
-    		if (a % 2 != 0) {
-    			a = (a + 1) / 2;
-    		} else {
-    			a = a / 2;
-    		}
-    		
-    		if (b % 2 != 0) {
-    			b = (b + 1) / 2;
-    		} else {
-    			b = b / 2;
-    		}
+import java.util.PriorityQueue;
 
-    		cnt++;
-    	}
-    	
-    	return cnt;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        
+    	PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+    	for (int num : scoville)
+        	pq.add(num);
+        
+        int count = 0;
+        int min = pq.peek();
+        while (min < K) {
+        	
+        	if (pq.size() > 1) {
+        		int first = pq.poll();
+        		int second = pq.poll();
+        		int newScoville = first + second * 2;
+        		pq.add(newScoville);
+        		
+        		min = pq.peek();
+        		count++;
+        	} else
+        		return -1;
+        }
+        
+        return count;
+        
     }
 }
