d5860190beff453b39325708bdb85925846e4854
오완진 <dhkswls1998@naver.com>
2024-09-09 17:49:27 +0900

    240909

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -1,164 +1,119 @@
 package baekjoon;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
 	
-	static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
-	static int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
 	
-	static int N, M, K;
-	static List<int[]> fbList;								// 전체 파이어볼 (유령볼 포함)
+	static int N, aliveTime;
+	static int[][] map;
+	static int[] shark;
+	static List<int[]>[] fishList;
 	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		
 		N = sc.nextInt();
-		M = sc.nextInt();
-		K = sc.nextInt();
-		fbList = new ArrayList<>();
+		map = new int[N][N];
+		fishList = new List[7];
+		for (int i = 1; i <= 6; i++)
+			fishList[i] = new ArrayList<>();
 		
-		for (int m = 0; m < M; m++) {
-			int[] fb = new int[5];
-			fb[0] = sc.nextInt() - 1;	// rNow
-			fb[1] = sc.nextInt() - 1;	// cNow
-			fb[2] = sc.nextInt();		// 질량m
-			fb[3] = sc.nextInt();		// 속력s
-			fb[4] = sc.nextInt();		// 방향d
-			
-			fbList.add(fb);
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < N; c++) {
+				int pos = sc.nextInt();
+				map[r][c] = pos;
+				if (pos == 9)
+					shark = new int[] {r, c, 2, 0};			// r, c, 레벨=필요경험치, 획득경험치
+				else if (1 <= pos && pos <= 6)
+					fishList[pos].add(new int[] {r, c});	// 섭취 -> {99, 99}
+			}
 		}//input
 		
-		for (int k = 0; k < K; k++) {
+		aliveTime = 0;
+		while (true) {
 			
-			boolean[][] visited = new boolean[N][N];		// contact 중복조회 방지
-			List<int[]> contactList = new ArrayList<>();	// contact 인덱스 조회용
-			Stack<Integer>[][] contact = new Stack[N][N];	// fb 합체 판단용
-			for (int n1 = 0; n1 < N; n1++)
-				for (int n2 = 0; n2 < N; n2++)
-					contact[n1][n2] = new Stack<>();
+			int rNowShark = shark[0];
+			int cNowShark = shark[1];
+			int sizeShark = shark[2];
+			int minTime = Integer.MAX_VALUE;
 			
+			// 탐색
+			for (int i = 1; i <= sizeShark; i++) {
+				List<int[]> fishListBySize = fishList[i];
+				
+				for (int[] fish : fishListBySize) {
+					
+					int rTargetFish = fish[0];
+					int cTargetFish = fish[1];
+					int time = 0;
+					
+					Queue<int[]> bfs = new LinkedList<>();
+					boolean[][] visited = new boolean[N][N];
+					
+					bfs.add(new int[] {rNowShark, cNowShark, time});
+					visited[rNowShark][cNowShark] = true;
+					
+					search:
+					while (!bfs.isEmpty()) {
+						
+						int[] nowShark = bfs.poll();
+						rNowShark = nowShark[0];
+						cNowShark = nowShark[1];
+						time = nowShark[2];
+						
+						for (int d = 0; d < 4; d++) {
+							int rNextShark = rNowShark + dr[d];
+							int cNextShark = cNowShark + dc[d];
+							
+							if (rNextShark < 0 || N <= rNextShark) continue;
+							if (cNextShark < 0 || N <= cNextShark) continue;
+							if (visited[rNextShark][cNextShark]) continue;
+							if (map[rNextShark][cNextShark] > sizeShark) continue;
+							
+							if (rNextShark == rTargetFish && cNextShark == cTargetFish) break search;
+							
+							bfs.add(new int[] {rNextShark, cNextShark, time + 1});
+							visited[rNextShark][cNextShark] = true;
+						}
+						
+					}//bfs
+					
+					/*
+					 * #####################   아이디어 정리   #####################
+					 * == 탐색
+					 * bfs 수정 -> 탐색하면서 가장 가까운 물고기를 찾으면 break
+					 * dr dc : 상좌하우 로 수정 필요
+					 * 
+					 * == 종료
+					 * 중간에 break search 가 안되고 종료된다면 엄마 소환
+					 * 
+					 * == 이동
+					 * map & List 둘 다 업데이트!
+					 * 
+					 * == 섭취
+					 * map -> 0 & List -> 99?
+					 * if (fish = 99?) continue 기저조건 추가?
+					 * 레벨업 = 필요경험치 충족 -> 크기+1 & 필요경험치 초기화
+					 */
+					
+				}//fish
+			}//fishListBySize
+			
+			// 종료
 			// 이동
-			for (int fbIdx = 0; fbIdx < fbList.size(); fbIdx++) {
-				int[] fb = fbList.get(fbIdx);
-				int rNow = fb[0];
-				int cNow = fb[1];
-				int m = fb[2];
-				int s = fb[3];
-				int d = fb[4];
-				
-				if (m == 0) continue;				// 유령볼(질량 0) 제외
-				
-				// 격자의 행과 열은 1번부터 N번까지 번호가 매겨져 있고,
-				// 1번 행은 N번과 연결되어 있고, 1번 열은 N번 열과 연결되어 있다.
-				// 이거 땜에 다시 푸는 중인데 한 번에 이해하신 분????
-				
-				int rNext = (rNow + dr[d] * s + 1000 * N) % N;	// N보다 s가 큰 경우(음수) 처리
-				int cNext = (cNow + dc[d] * s + 1000 * N) % N;
-				
-				contact[rNext][cNext].push(fbIdx);
-				contactList.add(new int[] {rNext, cNext});
-			}//이동
+			// 섭취
+			// 레벨업
 			
-			//합체
-			outer:
-			for (int[] check : contactList) {
-				int rCheck = check[0];
-				int cCheck = check[1];
-				
-				if (visited[rCheck][cCheck]) continue outer;
-				visited[rCheck][cCheck] = true;
-				
-				List<Integer> fbCheckList = new ArrayList<>();	// 합체 조건 만족한 fb 리스트
-				
-				while (!contact[rCheck][cCheck].isEmpty()) {
-					int fbIdx = contact[rCheck][cCheck].pop();
-					int[] fb = fbList.get(fbIdx);
-					
-					if (fb[2] == 0) continue;		// 유령볼(질량 0) 제외
-					
-					fbCheckList.add(fbIdx);
-				}
-				
-				int fbCnt = fbCheckList.size();
-				if (fbCnt == 1) {					// 이동만
-					int fbIdx = fbCheckList.get(0);
-					int[] fb = fbList.get(fbIdx);
-					fb[0] = rCheck;
-					fb[1] = cCheck;
-					
-					fbList.set(fbIdx, fb);
-				}
-				
-				else if (fbCnt > 1) {
-					
-					int sumM = 0;					// 질량합
-					int sumS = 0;					// 속력합
-					boolean isAllOdd = true;
-					boolean isAllEven = true;
-					
-					for (int fbIdx : fbCheckList) {
-						int[] fb = fbList.get(fbIdx);
-						int m = fb[2];
-						int s = fb[3];
-						int d = fb[4];
-						
-						sumM += m;
-						sumS += s;
-						if (d % 2 == 0) isAllOdd = false;
-						if (d % 2 != 0) isAllEven = false;
-						
-						fb[2] = 0;					// 기존것들 소멸
-						
-						fbList.set(fbIdx, fb);
-					}
-					
-					if (sumM < 5) continue outer;	// 유령볼(질량 0) 제외
-					
-					// 이동은 합체 단계에서 하지 않음
-					
-					if (isAllOdd || isAllEven) {
-						
-						for (int newIdx = 0; newIdx < 4; newIdx++) {
-							int[] fb = new int[5];	// 업데이트된 새로운 fb x4 생성
-							fb[0] = rCheck;
-							fb[1] = cCheck;
-							fb[2] = sumM / 5;
-							fb[3] = sumS / fbCnt;
-							fb[4] = newIdx * 2;
-							
-							fbList.add(fb);
-						}
-						
-					} else {
-						
-						for (int newIdx = 0; newIdx < 4; newIdx++) {
-							int[] fb = new int[5];	// 업데이트된 새로운 fb x4 생성
-							fb[0] = rCheck;
-							fb[1] = cCheck;
-							fb[2] = sumM / 5;
-							fb[3] = sumS / fbCnt;
-							fb[4] = newIdx * 2 + 1;
-							
-							fbList.add(fb);
-						}
-						
-					}// 방향 0246 or 1357
-					
-				}// if fbCnt > 1
-			}//합체
 			
-		}//K번
-		
-		int answer = 0;
-		for (int[] fb : fbList)
-			answer += fb[2];
-		
-		System.out.println(answer);
+		}
 		
 	}//main
 }//Main
