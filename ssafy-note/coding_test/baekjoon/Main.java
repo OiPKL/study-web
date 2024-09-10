f3d019ccc843738c72a85196bfdbd23ab20273ea
dhkswls1998 <dhkswls1998@naver.com>
2024-09-10 23:33:43 +0900

    240910

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -5,48 +5,84 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+	
+	static int N;
+	static int[] cows;
+	
 	public static void main(String[] args) throws IOException {
-		
+        Scanner sc = new Scanner(System.in);
+
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(reader.readLine().trim());
+        N = Integer.parseInt(reader.readLine().trim());
         String line = reader.readLine().trim();
         
         String[] tokens = line.split(" ");
-        int[] cows = new int[N];
+        cows = new int[N];
+
+        for (int i = 0; i < N; i++)
+        	cows[i] = Integer.parseInt(tokens[i]);
         
-        int K1 = 0;	// 첫번째로 큰 수
-        int K2 = 0;	// 두번째로 큰 수
-        List<Integer> K1List = new ArrayList<>();
-        List<Integer> K2List = new ArrayList<>();
+        // 아이디어
+        // maxHit : |i-j| * cows[i]
+        // maxHit 보다 커지려면 cows[i] 가 있는 |i-j| 밖이어야 함
+        // 인덱스의 왼쪽&오른쪽 끝까지 도달할때까지 가장 큰 cows[i] 찾기
+        
+        long minHit = Integer.MAX_VALUE;
         
         for (int i = 0; i < N; i++) {
-        	int cow = Integer.parseInt(tokens[i]);
-        	cows[i] = i;
         	
-        	if (cow > K1) {
-        		K1 = cow;
-        		K1List = new ArrayList<>();
-        		K1List.add(i);
-        	} else if (cow == K1)
-        		K1List.add(i);
+        	long maxHit = Integer.MIN_VALUE;
+    		
+    		for (int j = 0; j <= N - 1; j++) {
+    			if (cows[j] > biggestIce) {
+    				biggestIce = cows[j];
+    				biggestIceIdx = new ArrayList<>();
+    				biggestIceIdx.add(j);
+    			} else if (cows[i] == biggestIce) {
+    				biggestIceIdx.add(j);
+    			}
+    		}
+    		
+    		int farDistance = Integer.MIN_VALUE;
+    		int farIdx = 0;
+    		for (int idx : biggestIceIdx) {
+    			int distance = Math.abs(i - idx);
+    			if (distance >= farDistance) {
+    				farDistance = distance;
+    				farIdx = idx;
+    			}
+    		}
+    		
+    		int leftStart = 0;
+    		int leftEnd = ;
+    		int rightStart = ;
+    		int rightStart = N - 1;
+
+    		while (leftStart != leftEnd && rightStart != rightEnd) {
+    			
+    		}
+
+    		while (leftStart != leftEnd) {
+        		
+        	}
+    		
+    		while (rightStart != rightEnd) {
+    			
+    		}
+        	
         }
         
-        findK2:
-        for (int i = 0; i < N; i++) {
-        	for (int idx : K1List)
-        		if (idx == i) continue findK2;
-        	
-        	if (cows[i] > K2) {
-        		K2 = cows[i];
-        		K2List = new ArrayList<>();
-        		K2List.add(i);
-        	} else if (cows[i] == K2)
-        		K2List.add(i);
-        }
+        System.out.println(minHit);
+    }
+	
+	static List<Integer> getFarIdxList(int start, int end) {
 		
-		System.out.println();
+		int biggestIce = Integer.MIN_VALUE;
+		List<Integer> biggestIceIdx = new ArrayList<>();
 		
-	}//main
-}//Main
+		return biggestIceIdx
+	}
+}
