994dd5ffe3a9f04f02520f9c564999c89874e72f
dhkswls1998 <dhkswls1998@naver.com>
2024-09-12 23:18:01 +0900

    240912

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -1,10 +1,7 @@
 package baekjoon;
 
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
@@ -12,41 +9,49 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int TC = sc.nextInt();
-		for (int tc = 1; tc <= TC; tc++) {
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[] whenPassed = new int[N + 1];					// 이수학기
+		boolean[] studied = new boolean[N + 1];				// 수강완료
+		List<Integer>[] subject = new ArrayList[N + 1];
+		for(int n = 1; n <= N; n++)
+			subject[n] = new ArrayList<>();
+		
+		for (int m = 0; m < M; m++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			subject[b].add(a);
+		}
+		
+		int semester = 1;
+		while (true) {
 			
-	        Map<String, List<String>> removed = new HashMap<>();
-			PriorityQueue<String> minHeap = new PriorityQueue<>();
-			PriorityQueue<String> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+			List<Integer> picked = new ArrayList<>();	// 수강과목
 			
-			int K = sc.nextInt();
-			sc.nextLine();
-			
-			for (int k = 0; k < K; k++) {
-				String line = sc.nextLine();
-				char cmd = line.charAt(0);
-				char ops = line.charAt(2);
+			for (int n = 1; n <= N; n++) {
+				if (studied[n]) continue;
 				
-				if (cmd == 'I') {
-					if (ops == '-') {
-						
-					}
-				} else {
-					if (!minHeap.isEmpty()) {
-						if (ops == '-') {
-							
-						} else {
-							
-						}
-					}
+				if (subject[n].isEmpty()) {
+					picked.add(n);
+					whenPassed[n] = semester;
+					studied[n] = true;
 				}
 			}
 			
-		}//TC
+			if (picked.isEmpty()) break;
+			
+			for (int n = 1; n <= N; n++) {
+				if (studied[n]) continue;
+				
+				subject[n].removeAll(picked);
+			}
+			
+			semester++;
+		}
+		
+        for (int n = 1; n <= N; n++) {
+            System.out.print(whenPassed[n] + " ");
+        }
 	}
 }
-
-/*
- * 							removed.putIfAbsent("asdf", new ArrayList<>());
-							removed.get("key").add(123);
- */
