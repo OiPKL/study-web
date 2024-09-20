1ffb31889513ef77a5bccec6e396d72af74b0ec9
dhkswls1998 <dhkswls1998@naver.com>
2024-09-20 23:32:12 +0900

    240920

--- ssafy-note/coding_test/baekjoon_failed/G1_32136_소신발언/Main.java
+++ ssafy-note/coding_test/baekjoon_failed/G1_32136_소신발언/Main.java
@@ -23,72 +23,38 @@ public class Main {
 	       
 	       /*
 	        * 아이디어
-	        * maxMeltingTime 인 소의 위치를 maxCowL, maxCowR
 	        * 찾은 소의 위치로부터 히터의 위치가 가까워지면 갱신
 	        * 찾은 소의 위치로부터 히터의 위치가 멀어지면 볼 필요없음
 	        */
 	       
 	       int L = 0;
 	       int R = N - 1;
-	       int H = (L + R) / 2;
-	       int maxCowL = -1;
-	       int maxCowR = -1;
-	       long maxMeltingTimeL, maxMeltingTimeR;
-	       long minMaxMeltingTime = Long.MAX_VALUE;
+	       int oldH = L;
+	       int newH = (L + R) / 2;
 	       
-	       maxMeltingTimeL = 0;
+	       long maxMeltingTime = 0;
 	       for (int i = L; i <= R; i++) {
-	    	   long meltingTime = (i - L) * cows[i];
-	    	   maxMeltingTimeL = Math.max(maxMeltingTimeL, meltingTime);
+	    	   long meltingTime = Math.abs(i - oldH) * cows[i];
+	    	   maxMeltingTime = Math.max(maxMeltingTime, meltingTime);
 	       }
+	       long minMaxMeltingTime = maxMeltingTime;
 	       
-	       maxMeltingTimeR = 0;
-	       for (int i = R; i >= L; i--) {
-	    	   long meltingTime = (R - i) * cows[i];
-	    	   maxMeltingTimeR = Math.max(maxMeltingTimeR, meltingTime);
-	       }
-	       
-	       minMaxMeltingTime = Math.min(maxMeltingTimeL, maxMeltingTimeR);
-	       
-	       while (L <= R) {
+	       while (oldH <= newH) {
 	    	   
-	    	   maxMeltingTimeL = 0;
-	    	   for (int i = L; i <= H - 1; i++) {
-	    		   long meltingTime = (H - i) * cows[i];
-	    		   if (meltingTime >= maxMeltingTimeL) {
-	    			   maxMeltingTimeL = meltingTime;
-	    			   maxCowL = i;
-	    		   }
+	    	   maxMeltingTime = 0;
+	    	   for (int i = L; i <= R; i++) {
+		    	   long meltingTime = Math.abs(i - newH) * cows[i];
+		    	   maxMeltingTime = Math.max(maxMeltingTime, meltingTime);
+		       }
+	    	   
+	    	   if (minMaxMeltingTime < maxMeltingTime) {
+	    		   
 	    	   }
 	    	   
-	    	   maxMeltingTimeR = 0;
-	    	   for (int i = R; i >= H + 1; i--) {
-	    		   long meltingTime = (i - H) * cows[i];
-	    		   if (meltingTime >= maxMeltingTimeR) {
-	    			   maxMeltingTimeR = meltingTime;
-	    			   maxCowR = i;
-	    		   }
-	    	   }
+//	    	   newH = (oldH + newH) / 2; ???
+//	    	   
+//	    	   newH = (newH + R) / 2; ???
 	    	   
-	    	   if (maxMeltingTimeL < maxMeltingTimeR) {
-	    		   L = maxCowL;
-	    		   H = (L + R) / 2;
-	    		   if (minMaxMeltingTime > maxMeltingTimeR)
-	    			   minMaxMeltingTime = maxMeltingTimeR;
-	    		   else
-	    			   break;
-	    	   } else if (maxMeltingTimeR < maxMeltingTimeL) {
-	    		   R = maxCowR;
-	    		   H = (L + R) / 2;
-	    		   if (minMaxMeltingTime > maxMeltingTimeL)
-	    			   minMaxMeltingTime = maxMeltingTimeL;
-	    		   else
-	    			   break;
-	    	   } else {	// maxMeltingTimeL = maxMeltingTimeR
-	    		   minMaxMeltingTime = maxMeltingTimeL;
-	    		   break;
-	    	   }
-
 	       }
 	       
 	       System.out.println(minMaxMeltingTime);
