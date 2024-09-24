c2444a5abb06361b4e4d62c724de029a4a8982dd
dhkswls1998 <dhkswls1998@naver.com>
2024-09-24 22:45:59 +0900

    240924

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -3,18 +3,46 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main {
+	
+	static int N;
+	static int[] arr, dp;
+	static boolean[] visited;
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
+		N = sc.nextInt();
 		
-		long result = 0L;
+		arr = new int[N];
+		dp = new int[N];
+		visited = new boolean[N];
 		
+		for (int n = 0; n < N; n++) {
+			arr[n] = sc.nextInt();
+			dp[n] = 1;
+		}
 		
+		for (int n = 0; n < N; n++)
+			getDp(n);
 		
-		System.out.println(result);
+        int max = 0;
+        for (int n = 0; n < N; n++)
+            max = Math.max(max, getDp(n));
+		
+		System.out.println(max);
+	}
+	
+	static int getDp(int x) {
+		
+		if (visited[x])
+			return dp[x];
+		
+		for (int n = 0; n < x; n++)
+			if(arr[n] < arr[x])
+				dp[x] = Math.max(dp[x], getDp(n) + 1);
+		
+		visited[x] = true;
+		return dp[x];
 	}
 }
--- ssafy-note/coding_test/baekjoon_failed/G3_16235_나무재테크/Main.java
+++ ssafy-note/coding_test/baekjoon_failed/G3_16235_나무재테크/Main.java
@@ -1,21 +1,21 @@
 package baekjoon_failed.G3_16235_나무재테크;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 	
 	static int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
 	static int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
 	
-	static int N, M, K, cnt = 0;
+	static int N, M, K;
 	static int[][] foodMap, foodPlan;
-	static List<Integer> greenList;
-	static PriorityQueue<Integer>[][] treeAgeMap;
+	static Set<Integer> greenSet;
+	static List<Integer>[][] treeAgeMap;
 	
 	public static void main(String[] args) {
 		
@@ -34,64 +34,38 @@ public class Main {
 			}
 		}
 		
-		greenList = new ArrayList<>();			// 녹지목록(나무>=1 좌표목록), key: 10*r + c
-		treeAgeMap = new PriorityQueue[N][N];	// 나무나이지도
+		greenSet = new HashSet<>();				// 녹지목록(나무>=1 좌표목록), key: N*r + c
+		treeAgeMap = new ArrayList[N][N];		// 나무나이지도
 		for (int r = 0; r < N; r++)
 			for (int c = 0; c < N; c++)
-				treeAgeMap[r][c] = new PriorityQueue<>();
+				treeAgeMap[r][c] = new ArrayList<>();
 		
 		for (int m = 0; m < M; m++) {
 			int r = sc.nextInt() - 1;
 			int c = sc.nextInt() - 1;
 			treeAgeMap[r][c].add(sc.nextInt());
-			cnt++;
 			
-			int key = 10*r + c;
-			if (!greenList.contains(key))
-				greenList.add(key);
+			int key = N * r + c;
+			greenSet.add(key);
 		}
 		
 		for (int k = 1; k <= K; k++) {
 			
-			// ********************************************************************************************
-			System.out.println(k + "년: ");
-			System.out.println("foodMap");
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < N; c++)
-					System.out.print(foodMap[r][c] + " ");
-				System.out.println();
-			}
-			
-			System.out.println("treeAgeMap");
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < N; c++)
-					System.out.print(treeAgeMap[r][c].size() + " ");
-				System.out.println();
-			}
-			
-			System.out.println("greenList");
-			for (int num : greenList)
-				System.out.print(num + " ");
-			System.out.println();
-			// ********************************************************************************************
-			
 			// 봄 & 여름
-			List<Integer> newGreenList = new ArrayList<>();
-			for (int g = 0; g < greenList.size(); g++) {
-				int key = greenList.get(g);
-				int r = key / 10;
-				int c = key % 10;
+			Set<Integer> newGreenSet = new HashSet<>();
+			for (int key : greenSet) {
+				int r = key / N;
+				int c = key % N;
 				int food = foodMap[r][c];
 				
-				List<Integer> treeAgeList = new ArrayList<>();
-				Queue<Integer> newTreeAgeList = new LinkedList<>();
-				while (!treeAgeMap[r][c].isEmpty())
-					treeAgeList.add(treeAgeMap[r][c].poll());
+                List<Integer> treeAgeList = treeAgeMap[r][c];
+                Collections.sort(treeAgeList);
+                
+                List<Integer> newTreeAgeList = new ArrayList<>();
 				
 				int energy = 0;
 				boolean enough = true;
-				for (int ta = 0; ta < treeAgeList.size(); ta++) {
-					int treeAge = treeAgeList.get(ta);
+				for (int treeAge : treeAgeList) {
 					
 					if (enough) {
 						if (food >= treeAge) {
@@ -100,86 +74,59 @@ public class Main {
 						} else {
 							energy += treeAge / 2;
 							enough = false;
-							cnt--;
 						}
-					} else {
+					} else
 						energy += treeAge / 2;
-						cnt--;
-					}
 				}
 				
 				foodMap[r][c] = food + energy;
-				if (!newTreeAgeList.isEmpty()) {
-					newGreenList.add(key);
-					
-					while (!newTreeAgeList.isEmpty())
-						treeAgeMap[r][c].add(newTreeAgeList.poll());
-				}
+				
+			    treeAgeMap[r][c].clear(); 
+			    treeAgeMap[r][c].addAll(newTreeAgeList);
+				
+				if (!newTreeAgeList.isEmpty())
+					newGreenSet.add(key);
 			}
 			
-			greenList = new ArrayList<>();
-			greenList = List.copyOf(newGreenList);
-			
-			// ********************************************************************************************
-			System.out.print("여름: ");
-			for (int num : greenList)
-				System.out.print(num + " ");
-			System.out.println();
-			// ********************************************************************************************
+            greenSet = new HashSet<>(newGreenSet);
 			
 			// 가을
-			newGreenList = new ArrayList<>();
-			for (int g = 0; g < greenList.size(); g++) {
-				int key = greenList.get(g);
-				int r = key / 10;
-				int c = key % 10;
+            newGreenSet = new HashSet<>();
+            for (int key : greenSet) {
+				int r = key / N;
+				int c = key % N;
 
-				newGreenList.add(key);
+				newGreenSet.add(key);
 				
-				List<Integer> treeAgeList = new ArrayList<>();
-				while (!treeAgeMap[r][c].isEmpty())
-					treeAgeList.add(treeAgeMap[r][c].poll());
-				
-				for (int ta = 0; ta < treeAgeList.size(); ta++) {
-					int treeAge = treeAgeList.get(ta);
-					
+			    for (int treeAge : treeAgeMap[r][c]) {
 					if (treeAge % 5 == 0) {
-						
 						for (int d = 0; d < 8; d++) {
 							int rn = r + dr[d];
 							int cn = c + dc[d];
 							
-							if (rn < 0 || N <= rn) continue;
-							if (cn < 0 || N <= cn) continue;
+							if (rn < 0 || N <= rn || cn < 0 || N <= cn) continue;
 							
 							treeAgeMap[rn][cn].add(1);
-							cnt++;
 							
-							int keyN = rn * 10 + cn;
-							if (!greenList.contains(keyN))
-								newGreenList.add(keyN);
+							int keyN = rn * N + cn;
+							newGreenSet.add(keyN);
 						}
 					}
 				}
 			}
 			
-			greenList = new ArrayList<>();
-			greenList = List.copyOf(newGreenList);
-			
-			// ********************************************************************************************
-			System.out.print("가을: ");
-			for (int num : greenList)
-				System.out.print(num + " ");
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			// ********************************************************************************************
+            greenSet = new HashSet<>(newGreenSet);
 			
 			// 겨울
 			for (int r = 0; r < N; r++)
 				for (int c = 0; c < N; c++)
 					foodMap[r][c] += foodPlan[r][c];
 		}
+		
+		int cnt = 0;
+		for (int r = 0; r < N; r++)
+			for (int c = 0; c < N; c++)
+				cnt += treeAgeMap[r][c].size();
 		
 		System.out.println(cnt);
 	}
