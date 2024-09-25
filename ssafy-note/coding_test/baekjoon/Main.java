74e227fa7363600b75d8a2ed8914f0eb88260948
오완진 <dhkswls1998@naver.com>
2024-09-25 17:59:34 +0900

    240925

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -3,6 +3,10 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main {
+	
+	static int max, score, N;
+	static int[][] stk, dp;
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -11,16 +15,24 @@ public class Main {
 		
 		for (int tc = 1; tc <= TC; tc++) {
 			
-			int max = 0;
-			int score = 0;
-			int N = sc.nextInt();
-			int[][] stk = new int[2][N];
+			max = 0;
+			score = 0;
+			N = sc.nextInt();
+			stk = new int[2][N];
+			dp = new int[2][N];
 			
 			for (int r = 0; r < 2; r++)
 				for (int c = 0; c < N; c++)
 					stk[r][c] = sc.nextInt();
 			
+			dp[0][0] = stk[0][0];
+			dp[0][1] = stk[0][1];
 			
+			for (int r = 0; r < 2; r++) {
+				for (int c = 0; c < N; c++) {
+					
+				}
+			}
 			
 		}// tc
 	}
