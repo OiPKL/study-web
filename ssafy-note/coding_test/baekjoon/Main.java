6c57d50b5077eb29017b057618cbbae23a979d53
오완진 <dhkswls1998@naver.com>
2024-09-25 18:24:44 +0900

    240925

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 	
-	static int max, score, N;
+	static int N;
 	static int[][] stk, dp;
 	
 	public static void main(String[] args) {
@@ -15,25 +15,27 @@ public class Main {
 		
 		for (int tc = 1; tc <= TC; tc++) {
 			
-			max = 0;
-			score = 0;
 			N = sc.nextInt();
-			stk = new int[2][N];
-			dp = new int[2][N];
+			stk = new int[N][2];
+			dp = new int[N][2];
 			
-			for (int r = 0; r < 2; r++)
-				for (int c = 0; c < N; c++)
+			for (int r = 0; r < N; r++)
+				for (int c = 0; c < 2; c++)
 					stk[r][c] = sc.nextInt();
 			
 			dp[0][0] = stk[0][0];
 			dp[0][1] = stk[0][1];
 			
-			for (int r = 0; r < 2; r++) {
-				for (int c = 0; c < N; c++) {
-					
-				}
+			for (int r = 1; r < N; r++) {
+				dp[r][0] = Math.max(stk[r][0] + dp[r-1][1], Math.max(dp[r-1][0], dp[r-1][1]));
+				dp[r][1] = Math.max(stk[r][1] + dp[r-1][0], Math.max(dp[r-1][0], dp[r-1][1]));
 			}
 			
+			int maxScore1 = Math.max(dp[N-2][0], dp[N-2][1]);
+			int maxScore2 = Math.max(dp[N-1][0], dp[N-1][1]);
+			int maxScore = Math.max(maxScore1, maxScore2);
+			
+			System.out.println(maxScore);
 		}// tc
 	}
 }
