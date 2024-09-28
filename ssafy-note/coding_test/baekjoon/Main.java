6f1be4a7e0454142e404c85b922eacf954cc4480
dhkswls1998 <dhkswls1998@naver.com>
2024-09-28 22:29:17 +0900

    240928

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -3,19 +3,26 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		int W = sc.nextInt();
-		
-		int[] jadu = new int[T+1];
-		for (int t = 1; t <= T; t++)
-			jadu[t] = sc.nextInt();
-		
-		int[][] dp = new int[T+1][3];
-		
-		
-	}
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        int[][] works = new int[N + 1][2];
+        for (int i = 1; i <= N; i++) {
+            works[i][0] = sc.nextInt();	 // 상담기간
+            works[i][1] = sc.nextInt();	 // 상담보수
+        }
+        
+        int[] dp = new int[N + 2];
+
+        for (int i = 1; i <= N; i++) {
+        	if (i + works[i][0] <= N+1)
+        		dp[i + works[i][0]] = Math.max(dp[i + works[i][0]], dp[i] + works[i][1]);
+        	dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+
+        System.out.println(dp[N + 1]);
+    }
 }
