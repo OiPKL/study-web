6a316450cf68b316a53e4578ccc0d8b8233c87a3
오완진 <dhkswls1998@naver.com>
2024-10-02 09:09:33 +0900

    241002

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -1,7 +1,47 @@
 package baekjoon;
 
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class Main {
+
+	static int N, D;
+	static int[] rail;
+	static PriorityQueue<int[]> persons;
+	
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		rail = new int[200_000_000];
+		
+		for (int i = 0; i < N; i++)
+			persons = new PriorityQueue<>(
+					(a, b) -> {
+						if (a[0] != b[0])
+							return a[0] - b[0];
+						else
+							return a[1] - b[1];
+					});
+		
+		for (int i = 0; i < N; i++) {
+			int A = sc.nextInt() + 100_000_000;
+			int B = sc.nextInt() + 100_000_000;
+			
+			if (A <= B)
+				persons.add(new int[] {A, B});
+			else
+				persons.add(new int[] {B, A});
+		}
+		
+		D = sc.nextInt();
+		
+//		for (int i = 0; i < N; i++) {
+//			int[] person = persons.poll();
+//			System.out.println(person[0] + " " + person[1]);
+//		}
+		
 		
 	}
 }
