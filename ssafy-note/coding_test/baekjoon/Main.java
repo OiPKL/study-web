dd6a05f00a09719df7a7f2ef1237052a19e9bbca
오완진 <dhkswls1998@naver.com>
2024-10-02 17:50:25 +0900

    241002

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -1,62 +1,31 @@
 package baekjoon;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
-
-	static int N, D;
-	static int[] rail;
-	static PriorityQueue<int[]> persons;
-	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt();
+		int T = sc.nextInt();
+		int W = sc.nextInt();
+		int[] answer = new int[T+1];
+		for (int t = 1; t <= T; t++)
+			answer[t] = (sc.nextInt()) % 2;
 		
-		List<int[]> persons = new ArrayList<>();
-		for (int i = 0; i < N; i++) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			
-			if (A <= B)
-				persons.add(new int[] {A, B});
-			else
-				persons.add(new int[] {B, A});
+		// w 이동횟수
+		// w 짝수 = 1번나무
+		// w 홀수 = 2번나무
+		int[][] dp = new int[W+1][T+1];
+		for (int w = 1; w <= W; w++) {
+			for (int t = 1; t <= T; t++) {
+				if (w % 2 != answer[t])
+					dp[w][t] = Math.max(dp[w-1][t], dp[w][t-1] + 1);
+				else
+					dp[w][t] = dp[w-1][t];
+			}
 		}
 		
-		// 오른쪽좌표를 기준으로 오름차순 정렬
-		Collections.sort(persons, (a, b) -> a[1] - b[1]);
-		
-		int D = sc.nextInt();
-		
-		
-		
-		
-		N = sc.nextInt();
-		rail = new int[200_000_000];
-		
-		for (int i = 0; i < N; i++)
-			persons = new PriorityQueue<>(
-					(a, b) -> {
-						if (a[0] != b[0])
-							return a[0] - b[0];
-						else
-							return a[1] - b[1];
-					});
-		
-		
-		D = sc.nextInt();
-		
-//		for (int i = 0; i < N; i++) {
-//			int[] person = persons.poll();
-//			System.out.println(person[0] + " " + person[1]);
-//		}
-		
-		
+		System.out.println(dp[W][T]);
 	}
 }
