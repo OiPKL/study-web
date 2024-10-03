159e7bf7370551ade7b0ec68b0668bf65998798b
dhkswls1998 <dhkswls1998@naver.com>
2024-10-03 20:58:31 +0900

    241003

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -1,44 +1,55 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		int W = sc.nextInt();
-		int[] answer = new int[T+1];
-		for (int t = 1; t <= T; t++)
-			answer[t] = sc.nextInt();
-		
-		// w번 움직였을 때 t초까지의 자두 최대개수
-        int[][] dp = new int[W + 1][T + 1];
-
-        for (int t = 1; t <= T; t++) {
-            for (int w = 0; w <= W; w++) {
-                int picked = (w % 2) + 1;										// w -> 현재위치 추정가능
-
-                if (answer[t] == picked)
-                    dp[w][t] = dp[w][t - 1] + 1;								// 현재나무=정답 -> +1
-                else
-                    dp[w][t] = dp[w][t - 1];									// 현재나무=오답 -> 유지
-
-                if (w > 0) {
-                    int unpicked = (picked == 1) ? 2 : 1;
-                    if (answer[t] == unpicked)
-                        dp[w][t] = Math.max(dp[w][t], dp[w - 1][t - 1] + 1);	// 이동 후 현재나무=정답 -> +1
-                    else
-                        dp[w][t] = Math.max(dp[w][t], dp[w - 1][t - 1]);		// 이동 후 현재나무=오답 -> 유지
-                }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        String[] firstLine = br.readLine().split(" ");
+        int N1 = Integer.parseInt(firstLine[0]);
+        int N2 = Integer.parseInt(firstLine[1]);
+        
+        String[] titles = new String[N1];
+        int[] limits = new int[N1];
+        HashSet<Integer> check = new HashSet<>();
+        
+        int idx = 0;
+        for (int i = 0; i < N1; i++) {
+            String line = br.readLine();
+            String[] splits = line.split(" ");
+            int limit = Integer.parseInt(splits[1]);
+            
+            // 중복값 제거
+            // 이거로도 시간초과 떠서 버퍼드리더 가져옴
+            if (!check.contains(limit)) {
+                check.add(limit);
+                titles[idx] = splits[0];
+                limits[idx] = limit;
+                idx++;
             }
         }
-
-        int result = 0;
-        for (int w = 0; w <= W; w++)
-            result = Math.max(result, dp[w][T]);
-
-        System.out.println(result);
-	}
+        
+        for (int i = 0; i < N2; i++) {
+            int power = Integer.parseInt(br.readLine());
+            
+            int L = 0;
+            int R = idx - 1;
+            while (L <= R) {
+                int M = (L + R) / 2;
+                if (limits[M] < power)
+                    L = M + 1;
+                else
+                    R = M - 1;
+            }
+            
+            sb.append(titles[L]).append("\n");
+        }
+        
+        System.out.print(sb);
+    }
 }
