495e04ee7489a608020914a96c39615d5d9556c1
dhkswls1998 <dhkswls1998@naver.com>
2024-10-03 21:48:21 +0900

    241003

--- ssafy-note/coding_test/programmers/Solution.java
+++ ssafy-note/coding_test/programmers/Solution.java
@@ -1,26 +1,36 @@
 package programmers;
 
 class Solution {
-    public long solution(int n, int[] times) {
+    public int solution(int m, int n, int[][] puddles) {
     	
-    	long L = 0;
-    	long R = Long.MAX_VALUE;
+    	int[][] dp = new int[m+1][n+1];
     	
-    	while (L <= R) {
-    		long M = (L + R) / 2;
-    		long cnt = 0;
-    		
-    		for (int time : times) {
-    			cnt += M / time;
-    			if (cnt >= n) break;
-    		}
-    		
-    		if (cnt >= n)
-    			R = M - 1;
-    		else
-    			L = M + 1;
-    	}
+    	for (int[] puddle : puddles)
+    		dp[puddle[0]][puddle[1]] = -1;
     	
-    	return L;
+        dp[1][1] = 1;
+        
+        for (int i = 1; i <= m; i++) {
+        	if (dp[i][1] == -1) break;
+        	dp[i][1] = 1;
+        }
+        
+        for (int j = 1; j <= n; j++) {
+        	if (dp[1][j] == -1) break;
+        	dp[1][j] = 1;
+        }
+        
+        for (int i = 2; i <= m; i++) {
+        	for (int j = 2; j <= n; j++) {
+                if (dp[i][j] == -1) continue;
+                if (dp[i-1][j] != -1)
+                    dp[i][j] += dp[i-1][j];
+                if (dp[i][j-1] != -1)
+                    dp[i][j] += dp[i][j-1];
+                dp[i][j] %= 1_000_000_007;
+        	}
+        }
+
+        return dp[m][n];
     }
 }
