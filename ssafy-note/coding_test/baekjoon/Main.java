bef44f81cfd2ca95a7593c781890337dd3a81160
dhkswls1998 <dhkswls1998@naver.com>
2024-10-09 00:04:06 +0900

    241009

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -1,50 +1,80 @@
 package baekjoon;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	
-	static int mod = 1_000_000_007;
-
-	static int N, result;
-	static int[] menu;
-	static boolean[] visited;
+	static int[][][] hole = new int[][][] {
+		{{0, 1}, {2, 3}}, {{1, 0}, {3, 2}}, 
+		{{2, 3}, {0, 1}}, {{3, 2}, {1, 0}}
+	};
 	
-	public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 		
-		Scanner sc = new Scanner(System.in);
-	
-		N = sc.nextInt();
-
-		menu = new int[N];
-		for (int i = 0; i < N; i++)
-			menu[i] = sc.nextInt();
-		visited = new boolean[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
-		Arrays.sort(menu);
+        int K = Integer.parseInt(br.readLine().trim());
+        int N = (int) Math.pow(2, K);
 		
-		result = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int H = Integer.parseInt(br.readLine().trim());
 		
-		btk(0, 0, 0);
+		/*
+		 * hole[]
+		 * [0][0] [0][1]
+		 * [1][0] [1][1]
+		 * 
+		 * DU flse RL flse	-> [0][0]
+		 * DU flse RL true	-> [0][1]
+		 * DU true RL flse	-> [1][0]
+		 * RL true DU true	-> [1][1]
+		 */
 		
-		System.out.println(result);
-	}
-	
-	static void btk(int idx, int cnt, int sum) {
+		boolean DU = false;
+		boolean RL = false;
+        while (st.hasMoreTokens()) {
+            String cmd = st.nextToken();
+            if (cmd.equals("D"))
+                DU = true;
+            else if (cmd.equals("U"))
+                DU = false;
+            else if (cmd.equals("R"))
+                RL = true;
+            else
+                RL = false;
+        }
 		
-		System.out.println(idx + " | " + cnt + " | " + sum);
-		System.out.println(Arrays.toString(visited));
+		int[] picked = new int[2];
 		
-		if (cnt > 1)
-			result = (result + sum) % mod;
+		if (RL) {
+			if (DU)
+				picked = new int[] {1, 1};
+			else
+				picked = new int[] {0, 1};
+		} else if (DU)
+			picked = new int[] {1, 0};
 		
-		for (int i = idx; i < N; i++) {
-			if (!visited[i]) {
-				visited[i] = true;
-				btk(i, cnt + 1, sum + menu[i]);
-				visited[i] = false;
+		int tmpR = picked[0];
+		int tmpC = picked[1];
+		int holeNum = -1;
+		for (int i = 0; i < 4; i++)
+			if (hole[i][tmpR][tmpC] == H)
+				holeNum = i;
+		
+		int[][] copy = hole[holeNum];
+				
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c <N; c++) {
+				sb.append(copy[r%2][c%2]).append(" ");
 			}
+			sb.append("\n");
 		}
+		
+		System.out.println(sb);
 	}
 }
