498743b1e1e897fa56c37498e41ca9591b4c7b62
오완진 <dhkswls1998@naver.com>
2024-10-08 17:58:53 +0900

    241008

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -1,71 +1,50 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	
-	static int[][][] hole = new int[][][] {
-		{{0, 1}, {2, 3}}, {{1, 0}, {3, 2}}, 
-		{{2, 3}, {0, 1}}, {{3, 2}, {1, 0}}
-	};
+	static int mod = 1_000_000_007;
+
+	static int N, result;
+	static int[] menu;
+	static boolean[] visited;
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+	
+		N = sc.nextInt();
+
+		menu = new int[N];
+		for (int i = 0; i < N; i++)
+			menu[i] = sc.nextInt();
+		visited = new boolean[N];
 		
-		int K = sc.nextInt();
-		sc.nextLine();
+		Arrays.sort(menu);
 		
-		int N = (int) Math.pow(2, K);
+		result = 0;
 		
-		String line = sc.nextLine();
-		String[] cmd = line.split(" ");
+		btk(0, 0, 0);
 		
-		int H = sc.nextInt();
+		System.out.println(result);
+	}
+	
+	static void btk(int idx, int cnt, int sum) {
 		
-		/*
-		 * [0][0] [0][1]
-		 * [1][0] [1][1]
-		 * 
-		 * 기준				-> [0][0]
-		 * RL true 			-> [0][1]
-		 * DU true 			-> [1][0]
-		 * RL true DU true	-> [1][1]
-		 */
+		System.out.println(idx + " | " + cnt + " | " + sum);
+		System.out.println(Arrays.toString(visited));
 		
-		boolean DU = false;
-		boolean RL = false;
-		for (int q = 0; q < cmd.length; q++) {
-			if (cmd[q].equals("D"))			DU = true;
-			else if (cmd[q].equals("U")) 	DU = false;
-			else if (cmd[q].equals("R")) 	RL = true;
-			else 							RL = false;
-		}
+		if (cnt > 1)
+			result = (result + sum) % mod;
 		
-		int[] picked = new int[2];
-		
-		if (RL) {
-			if (DU)
-				picked = new int[] {1, 1};
-			else
-				picked = new int[] {0, 1};
-		} else if (DU)
-			picked = new int[] {1, 0};
-		
-		int tmpR = picked[0];
-		int tmpC = picked[1];
-		int holeNum = -1;
-		for (int i = 0; i < 4; i++)
-			if (hole[i][tmpR][tmpC] == H)
-				holeNum = i;
-		
-		int[][] copy = hole[holeNum];
-				
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c <N; c++) {
-				System.out.print(copy[r%2][c%2] + " ");
+		for (int i = idx; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				btk(i, cnt + 1, sum + menu[i]);
+				visited[i] = false;
 			}
-			System.out.println();
 		}
 	}
 }
