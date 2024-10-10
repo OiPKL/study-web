6e2ae025578f7c0aaf78f28c8d05c62f3b0bde4b
오완진 <dhkswls1998@naver.com>
2024-10-10 14:33:13 +0900

    241010

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -15,13 +15,16 @@ public class Main {
 		
 		// k개 pick ~ 합이 n
 		int[][] dp = new int[K+1][N+1];
-		dp[0][0] = 1;
+		
+		for (int d = 0; d <= K; d++)
+			dp[d][0] = 1;
+		
 		// 머리 빠개지는 중............
 		for (int d = 1; d <= K; d++)
-			for (int p = d; p <= N; p++)
-				for (int i = 0; i <= d - 1; i++)
+			for (int p = 1; p <= N; p++)
+				for (int i = 0; i <= d; i++)
 					for (int j = 0; j <= p; j++)
-						dp[d][p] += dp[i][j];		// --> [o-o] 로 수정해야함
+						
 					
 		for (int d = 0; d <= K; d++) {
 			for (int p = 0; p <= N; p++)
