7bc02771a81548b4af3575fa17a7eafbfab6a0e5
dhkswls1998 <dhkswls1998@naver.com>
2024-10-10 22:03:40 +0900

    241010

--- ssafy-note/coding_test/baekjoon/Main.java
+++ ssafy-note/coding_test/baekjoon/Main.java
@@ -13,27 +13,23 @@ public class Main {
 		int N = sc.nextInt();
 		int K = sc.nextInt();
 		
-		// k개 pick ~ 합이 n
 		int[][] dp = new int[K+1][N+1];
 		
-		for (int d = 0; d <= K; d++)
-			dp[d][0] = 1;
-		
-		for (int p = 0; p <= N; p++)
-			dp[1][p] = 1;
-		
-		// 머리 빠개지는 중............
-		for (int d = 2; d <= K; d++)
-			for (int p = 1; p <= N; p++)
-				for (int i = 1; i <= d; i++)
-					for (int j = 0; j <= p; j++)
-						dp[d][p] += (dp[i][j] * dp[d-i][p-j]) % mod;
+        for (int k = 0; k <= K; k++)
+            dp[k][0] = 1;
+
+        for (int n = 0; n <= N; n++)
+            dp[1][n] = 1;
+
+        for (int k = 2; k <= K; k++)
+            for (int n = 1; n <= N; n++)
+                dp[k][n] = (dp[k-1][n] + dp[k][n-1]) % mod;
 					
-		for (int d = 0; d <= K; d++) {
-			for (int p = 0; p <= N; p++)
-				System.out.printf("%3d" + " ", dp[d][p]);
-			System.out.println();
-		}
+//		for (int d = 0; d <= K; d++) {
+//			for (int p = 0; p <= N; p++)
+//				System.out.printf("%3d" + " ", dp[d][p]);
+//			System.out.println();
+//		}
 		
 		System.out.println(dp[K][N]);
 	}
