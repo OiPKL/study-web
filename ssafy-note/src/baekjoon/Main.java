c2a041d11e43eb1a73d5715c3d7ac4401d894b6f
오완진 <dhkswls1998@naver.com>
2024-10-17 11:03:34 +0900

    241017

--- ssafy-note/src/baekjoon/Main.java
+++ ssafy-note/src/baekjoon/Main.java
@@ -1,75 +1,27 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-	
-	static int N, B, C;
-	static int[] cows;
-	
-    public static void main(String[] args) throws IOException {
-    	
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        N = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        
-        cows = new int[N];
-        st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
 
-        for (int i = 0; i < N; i++)
-            cows[i] = Integer.parseInt(st.nextToken());
+    	Scanner sc = new Scanner(System.in);
     	
-    	Arrays.sort(cows);
+    	int N = sc.nextInt();
+    	int M = sc.nextInt();
+    	int K = sc.nextInt();
+    	sc.nextLine();
     	
-    	int L = 0;			// 불가능
-    	int R = cows[N-1];	// 가능
-    	int answer = R;
-    	
-    	while (L <= R) {
-    		
-    		int M = (L + R) / 2;
-    		
-    		if (canCarry(M)) {
-    			answer = M;
-    			R = M - 1;
-    		}
-    		else
-    			L = M + 1;
-    	}
-    	
-    	System.out.println(answer);
-    }
-    
-    static boolean canCarry(int M) {
-    	
-    	int idx = 0;
-    	int cntBus = 0;
-    	
-    	while (idx < N) {
-    		
-    		cntBus++;
-    		
-    		int time = cows[idx];
-    		int cntCow = 0;
-    		
-    		while (idx < N) {
-    			// 정원 초과 ~ 버스 출발
-    			if (cntCow >= C) break;
-    			// 시간 초과 ~ 버스 출발
-    			if (cows[idx] - time > M) break;
+    	char[][] map = new char[N][M];
+    	for (int n = 0; n < N; n++)
+    		map[n] = sc.nextLine().toCharArray();
 
-    			idx++;
-    			cntCow++;
-    		}
-    	}
+    	List<char[]> word = new ArrayList<>();
+    	for (int k = 0; k < K; k++)
+    		word.add(sc.nextLine().toCharArray());
     	
-    	return cntBus <= B;
+    	
     }
 }
