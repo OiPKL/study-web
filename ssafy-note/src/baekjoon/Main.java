de312a39eab57089a3b1348ba71d2a7a1de2ced5
dhkswls1998 <dhkswls1998@naver.com>
2024-10-20 17:55:19 +0900

    241020

--- ssafy-note/src/baekjoon/Main.java
+++ ssafy-note/src/baekjoon/Main.java
@@ -1,163 +1,175 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 	
-	static int[][] dice = {{0, 2, 0},
-						   {4, 1, 3},
-						   {0, 5, 0},
-						   {0, 6, 0}};
-	
 	static int[] dn = {-1, 0, 1, 0};
 	static int[] dm = {0, 1, 0, -1};
 	
-	static int N, M, K, dWay = 1;
-	static int[][] map;
-	
-	/*
-	 * 이동방향 1칸 .. 못움직인다면 반대방향 1칸
-	 * 점수 계산 ~ map B * 개수 C
-	 * dice A > map B : +90도
-	 * dice A < map B : -90도
-	 * dice A = map B : 0도
-	 */
+	static int N, M, number, answer;
+	static int[][] bada;
+	static boolean[][] visited1;
+	static List<List<int[]>> islandInfos;
+	static int[][] bridges;
 	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		N = sc.nextInt();
 		M = sc.nextInt();
-		K = sc.nextInt();
+		number = 2;
 		
-		map = new int[N][M];
+		bada = new int[N][M];
 		for (int n = 0; n < N; n++)
 			for (int m = 0; m < M; m++)
-				map[n][m] = sc.nextInt();
+				bada[n][m] = sc.nextInt();
 		
-		int nNow = 0;
-		int mNow = 0;
-		int score = 0;
-		for (int k = 0; k < K; k++) {
-			
-			int nNext = nNow + dn[dWay];
-			int mNext = mNow + dm[dWay];
-			
-			if (nNext < 0 || N <= nNext || mNext < 0 || M <= mNext) {
-				dWay = (dWay + 2) % 4;
-				nNext = nNow + dn[dWay];
-				mNext = mNow + dm[dWay];
-			}
-			
-			// 주사위 이동
-			nNow = nNext;
-			mNow = mNext;
-			
-			// 주사위 굴리기
-			moveDice(dWay);
-			
-			// 점수 계산
-			score += map[nNow][mNow] * getCnt(nNow, mNow);
-			
-			// 다음 방향
-			nextDirection(nNow, mNow);
-		}
+		islandInfos = new ArrayList<>();
+		visited1 = new boolean[N][M];
+		for (int n = 0; n < N; n++)
+			for (int m = 0; m < M; m++)
+				if (bada[n][m] == 1 && !visited1[n][m])
+					findIsland(n, m);
 		
-		System.out.println(score);
+		bridges = new int[number][number];
+		buildBridge();
+		
+//		System.out.println();
+//		for (int n = 0; n < N; n++) {
+//			for (int m = 0; m < M; m++) {
+//				System.out.print(bada[n][m] + " ");
+//			}
+//			System.out.println();
+//		}
+//		
+//		System.out.println();
+//		for (int n = 2; n < number; n++) {
+//			for (int m = 2; m < number; m++) {
+//				System.out.print(bridges[n][m] + " ");
+//			}
+//			System.out.println();
+//		}
+
+        answer = 0;
+        calMinCost();
+        System.out.println(answer);
 	}
-	
-	static void moveDice(int dWay) {
-		switch(dWay) {
-		case 0:
-			moveN();
-			break;
-		case 1:
-			moveE();
-			break;
-		case 2:
-			moveS();
-			break;
-		case 3:
-			moveW();
-			break;
-		}
-	}
-	
-	static int getCnt(int n, int m) {
+
+	static void findIsland(int n, int m) {
 		
-		int num = map[n][m];
+		List<int[]> islandInfo = new ArrayList<>();
 		
 		Queue<int[]> bfs = new LinkedList<>();
-		boolean[][] visited = new boolean[N][M];
-		
 		bfs.add(new int[] {n, m});
-		visited[n][m] = true;
+		visited1[n][m] = true;
 		
-		int cnt = 0;
+		islandInfo.add(new int[] {number});
+		
 		while (!bfs.isEmpty()) {
-		
+			
 			int[] now = bfs.poll();
 			int nNow = now[0];
 			int mNow = now[1];
-			cnt++;
+			bada[nNow][mNow] = number;
+			islandInfo.add(new int[] {nNow, mNow});
 			
 			for (int d = 0; d < 4; d++) {
 				int nNext = nNow + dn[d];
 				int mNext = mNow + dm[d];
 				
 				if (nNext < 0 || N <= nNext || mNext < 0 || M <= mNext ||
-						visited[nNext][mNext] || map[nNext][mNext] != num) continue;
+					bada[nNext][mNext] == 0 || visited1[nNext][mNext]) continue;
 				
 				bfs.add(new int[] {nNext, mNext});
-				visited[nNext][mNext] = true;
+				visited1[nNext][mNext] = true;
 			}
 		}
 		
-		return cnt;
+		number++;
+		islandInfos.add(islandInfo);
 	}
 	
-	static void nextDirection(int n, int m) {
-		int diceNum = dice[3][1];
-		int mapNum = map[n][m];
+	static void buildBridge() {
 		
-		if (diceNum > mapNum)
-			dWay = (dWay + 1) % 4;
-		else if (diceNum < mapNum)
-			dWay = (dWay + 3) % 4;
+		for (List<int[]> islandInfo : islandInfos) {
+			
+			int islandNo = islandInfo.get(0)[0];
+			for (int i = 1; i < islandInfo.size(); i++) {
+				
+				int[] now = islandInfo.get(i);
+				int nNow = now[0];
+				int mNow = now[1];
+				
+				for (int d = 0; d < 4; d++) {
+	                int nNext = nNow;
+	                int mNext = mNow;
+					int cnt = 0;
+					
+					while (true) {
+	                    nNext += dn[d];
+	                    mNext += dm[d];
+	                    
+	                    if (nNext < 0 || N <= nNext || mNext < 0 || M <= mNext ||
+	                    	bada[nNext][mNext] == islandNo) break;
+	                    
+	                    if (bada[nNext][mNext] != 0) {
+	                        if (cnt >= 2) {
+	                            int nextIslandNo = bada[nNext][mNext];
+	                            if (bridges[islandNo][nextIslandNo] == 0)
+	                            	bridges[islandNo][nextIslandNo] = cnt;
+	                            else
+	                            	bridges[islandNo][nextIslandNo] = Math.min(bridges[islandNo][nextIslandNo], cnt);
+	                        }
+	                        break;
+	                    }
+	                    cnt++;
+	                }
+				}
+			}
+		}
 	}
 	
-	static void moveN() {
-		int tmp = dice[0][1];
-		dice[0][1] = dice[1][1];
-		dice[1][1] = dice[2][1];
-		dice[2][1] = dice[3][1];
-		dice[3][1] = tmp;
-	}
-	
-	static void moveE() {
-		int tmp = dice[1][2];
-		dice[1][2] = dice[1][1];
-		dice[1][1] = dice[1][0];
-		dice[1][0] = dice[3][1];
-		dice[3][1] = tmp;
-	}
-	
-	static void moveS() {
-		int tmp = dice[3][1];
-		dice[3][1] = dice[2][1];
-		dice[2][1] = dice[1][1];
-		dice[1][1] = dice[0][1];
-		dice[0][1] = tmp;
-	}
-	
-	static void moveW() {
-		int tmp = dice[1][0];
-		dice[1][0] = dice[1][1];
-		dice[1][1] = dice[1][2];
-		dice[1][2] = dice[3][1];
-		dice[3][1] = tmp;
-	}
+    static void calMinCost() {
+    	
+    	boolean[] visited2 = new boolean[number];
+    	int[] minCosts = new int[number];
+    	Arrays.fill(minCosts, Integer.MAX_VALUE);
+    	minCosts[2] = 0;
+    	
+    	for (int i = 2; i < number; i++) {
+    		int islandNo = -1;
+    		int minCost = Integer.MAX_VALUE;
+    		
+    		// 미방문 섬 중에서 최소비용 섬 찾기
+    		for (int j = 2; j < number; j++) {
+    			if (!visited2[j] && minCosts[j] < minCost) {
+    				islandNo = j;
+    				minCost = minCosts[j];
+    			}
+    		}
+    		
+    		// 모든 섬 연결 실패
+            if (islandNo == -1) {
+            	answer = -1;
+            	return;
+            }
+    		
+    		visited2[islandNo] = true;
+    		answer += minCost;
+    		
+    		// 선택한 섬과 연결된 섬들 비용 업데이트
+    		for (int j = 2; j < number; j++) {
+    			if (!visited2[j] && bridges[islandNo][j] != 0) {
+    				minCosts[j] = Math.min(minCosts[j], bridges[islandNo][j]);
+    			}
+    		}
+    	}
+    }
 }
