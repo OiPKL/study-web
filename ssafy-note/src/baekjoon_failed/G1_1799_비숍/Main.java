00299b2bafbf86a0fb1fea640b71317be405c3b1
dhkswls1998 <dhkswls1998@naver.com>
2024-10-23 23:05:37 +0900

    241023

--- ssafy-note/src/baekjoon_failed/G1_1799_비숍/Main.java
+++ ssafy-note/src/baekjoon_failed/G1_1799_비숍/Main.java
@@ -3,14 +3,13 @@ package baekjoon_failed.G1_1799_비숍;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int N, maxCnt;
+	
+	static int N, maxCntB, maxCntW;
 	static int[][] map;
-	static boolean[] evenSumCheck, evenChaCheck, oddSumCheck, oddChaCheck;
+	static boolean[] sumCheck, chaCheck;
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -25,124 +24,51 @@ public class Main {
 			for (int c = 0; c < N; c++)
 				map[r][c] = Integer.parseInt(st.nextToken());
 		}
-
-		evenSumCheck = new boolean[20];	// 0 2 4 6 8 10 12 14 16 18
-		evenChaCheck = new boolean[20];	// -8 -6 -4 -2 0 2 4 6 8	-> 0 2 4 6 8 10 12 14 16 18 (+8)
-		oddSumCheck = new boolean[20];	// 1 3 5 7 9 11 13 15 17
-		oddChaCheck = new boolean[20];	// -9 -7 -5 -3 -1 1 3 5 7 9	-> 1 3 5 7 9 11 13 15 17 (+10)
 		
-		maxCnt = 0;
-		btk(0, 0, 0);
+		maxCntB = 0;
+		maxCntW = 0;
+        sumCheck = new boolean[2 * N];
+        chaCheck = new boolean[2 * N];
 		
-		System.out.println(maxCnt);
+		btk(0, 0, 0, true);		// 합 짝수(Black)
+		btk(0, 1, 0, false);	// 합 홀수(White)
+		
+		System.out.println(maxCntB + maxCntW);
 	}
-
-	private static void btk(int rNow, int cNow, int cnt) {
+	
+	static void btk(int rNow, int cNow, int cnt, boolean isBlack) {
 		
 		if (rNow == N) {
-			maxCnt = Math.max(maxCnt, cnt);
+			if (isBlack)
+				maxCntB = Math.max(maxCntB, cnt);
+			else
+				maxCntW = Math.max(maxCntW, cnt);
 			return;
 		}
 		
-		if (map[rNow][cNow] == 1) {
-			if ((rNow + cNow) % 2 == 0) {
+		for (int r = rNow; r < N; r++) {
+			for (int c = r == rNow ? cNow : 0; c < N; c += 2) {
 				
-				for (int c = cNow; c < N; c++) {
+				if (map[r][c] == 1) {
+
+					int sum = r + c;
+					int cha = r - c + N;
 					
-					int sumCheck = rNow + cNow;
-					int chaCheck = rNow - cNow + 8;
-					
-					if (!evenSumCheck[sumCheck] && !evenChaCheck[chaCheck]) {
+					if (!sumCheck[sum] && !chaCheck[cha]) {
 						
-						evenSumCheck[sumCheck] = true;
-						evenChaCheck[chaCheck] = true;
-						if (cNow != N - 1)
-							btk(rNow, cNow + 1, cnt + 1);
+						sumCheck[sum] = true;
+						chaCheck[cha] = true;
+						if (c + 2 == N)
+							btk(r + 1, 1, cnt + 1, isBlack);
+						else if (c + 2 == N + 1)
+							btk(r + 1, 0, cnt + 1, isBlack);
 						else
-							btk(rNow + 1, 0, cnt + 1);
-						evenSumCheck[sumCheck] = false;
-						evenChaCheck[chaCheck] = false;
-					}
-				}
-				
-				for (int r = rNow + 1; r < N; r++) {
-					for (int c = 0; c < N; c++) {
-						
-						int sumCheck = r + cNow;
-						int chaCheck = r - cNow + 8;
-						
-						if (!evenSumCheck[sumCheck] && !evenChaCheck[chaCheck]) {
-							
-							evenSumCheck[sumCheck] = true;
-							evenChaCheck[chaCheck] = true;
-							if (cNow != N - 1)
-								btk(r, cNow + 1, cnt + 1);
-							else
-								btk(r + 1, 0, cnt + 1);
-							evenSumCheck[sumCheck] = false;
-							evenChaCheck[chaCheck] = false;
-						}
-					}
-				}
-			} else {
-				
-				for (int c = cNow; c < N; c++) {
-					
-					int sumCheck = rNow + cNow;
-					int chaCheck = rNow - cNow + 10;
-					
-					if (!oddSumCheck[sumCheck] && !oddChaCheck[chaCheck]) {
-						
-						oddSumCheck[sumCheck] = true;
-						oddChaCheck[chaCheck] = true;
-						if (cNow != N - 1)
-							btk(rNow, cNow + 1, cnt + 1);
-						else
-							btk(rNow + 1, 0, cnt + 1);
-						oddSumCheck[sumCheck] = false;
-						oddChaCheck[chaCheck] = false;
-					}
-				}
-				
-				for (int r = rNow + 1; r < N; r++) {
-					for (int c = 0; c < N; c++) {
-						
-						int sumCheck = r + cNow;
-						int chaCheck = r - cNow + 10;
-						
-						if (!oddSumCheck[sumCheck] && !oddChaCheck[chaCheck]) {
-							
-							oddSumCheck[sumCheck] = true;
-							oddChaCheck[chaCheck] = true;
-							if (cNow != N - 1)
-								btk(r, cNow + 1, cnt + 1);
-							else
-								btk(r + 1, 0, cnt + 1);
-							oddSumCheck[sumCheck] = false;
-							oddChaCheck[chaCheck] = false;
-						}
+							btk(r, c + 2, cnt + 1, isBlack);
+						sumCheck[sum] = false;
+						chaCheck[cha] = false;
 					}
 				}
 			}
-		}//map==1
-		
-		else {
-			
-			for (int c = cNow; c < N; c++) {
-				if (cNow != N - 1)
-					btk(rNow, cNow + 1, cnt);
-				else
-					btk(rNow + 1, 0, cnt);
-			}
-			
-			for (int r = rNow + 1; r < N; r++) {
-				for (int c = 0; c < N; c++) {
-					if (cNow != N - 1)
-						btk(r, cNow + 1, cnt);
-					else
-						btk(r + 1, 0, cnt);
-				}
-			}
-		}//map==0
+		}
 	}
 }
