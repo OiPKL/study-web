efd1e09a8aa193a1aa0a96ac2a524281d4857a91
오완진 <dhkswls1998@naver.com>
2024-10-23 17:56:41 +0900

    241023

--- ssafy-note/src/baekjoon_failed/G1_1799_비숍/Main.java
+++ ssafy-note/src/baekjoon_failed/G1_1799_비숍/Main.java
@@ -10,7 +10,6 @@ public class Main {
 
 	static int N, maxCnt;
 	static int[][] map;
-	static boolean[][] visited;
 	static boolean[] evenSumCheck, evenChaCheck, oddSumCheck, oddChaCheck;
 	
 	public static void main(String[] args) throws IOException {
@@ -27,7 +26,6 @@ public class Main {
 				map[r][c] = Integer.parseInt(st.nextToken());
 		}
 
-		visited = new boolean[N][N];
 		evenSumCheck = new boolean[20];	// 0 2 4 6 8 10 12 14 16 18
 		evenChaCheck = new boolean[20];	// -8 -6 -4 -2 0 2 4 6 8	-> 0 2 4 6 8 10 12 14 16 18 (+8)
 		oddSumCheck = new boolean[20];	// 1 3 5 7 9 11 13 15 17
@@ -40,11 +38,6 @@ public class Main {
 	}
 
 	private static void btk(int rNow, int cNow, int cnt) {
-	
-		System.out.println();
-		for (int r = 0; r < N; r++)
-			System.out.println(Arrays.toString(visited[r]));
-		System.out.println();
 		
 		if (rNow == N) {
 			maxCnt = Math.max(maxCnt, cnt);
@@ -59,16 +52,14 @@ public class Main {
 					int sumCheck = rNow + cNow;
 					int chaCheck = rNow - cNow + 8;
 					
-					if (!visited[rNow][cNow] && !evenSumCheck[sumCheck] && !evenChaCheck[chaCheck]) {
+					if (!evenSumCheck[sumCheck] && !evenChaCheck[chaCheck]) {
 						
-						visited[rNow][cNow] = true;
 						evenSumCheck[sumCheck] = true;
 						evenChaCheck[chaCheck] = true;
 						if (cNow != N - 1)
 							btk(rNow, cNow + 1, cnt + 1);
 						else
 							btk(rNow + 1, 0, cnt + 1);
-						visited[rNow][cNow] = false;
 						evenSumCheck[sumCheck] = false;
 						evenChaCheck[chaCheck] = false;
 					}
@@ -77,19 +68,17 @@ public class Main {
 				for (int r = rNow + 1; r < N; r++) {
 					for (int c = 0; c < N; c++) {
 						
-						int sumCheck = rNow + cNow;
-						int chaCheck = rNow - cNow + 8;
+						int sumCheck = r + cNow;
+						int chaCheck = r - cNow + 8;
 						
-						if (!visited[rNow][cNow] && !evenSumCheck[sumCheck] && !evenChaCheck[chaCheck]) {
+						if (!evenSumCheck[sumCheck] && !evenChaCheck[chaCheck]) {
 							
-							visited[rNow][cNow] = true;
 							evenSumCheck[sumCheck] = true;
 							evenChaCheck[chaCheck] = true;
 							if (cNow != N - 1)
 								btk(r, cNow + 1, cnt + 1);
 							else
 								btk(r + 1, 0, cnt + 1);
-							visited[rNow][cNow] = false;
 							evenSumCheck[sumCheck] = false;
 							evenChaCheck[chaCheck] = false;
 						}
@@ -102,16 +91,14 @@ public class Main {
 					int sumCheck = rNow + cNow;
 					int chaCheck = rNow - cNow + 10;
 					
-					if (!visited[rNow][cNow] && !oddSumCheck[sumCheck] && !oddChaCheck[chaCheck]) {
+					if (!oddSumCheck[sumCheck] && !oddChaCheck[chaCheck]) {
 						
-						visited[rNow][cNow] = true;
 						oddSumCheck[sumCheck] = true;
 						oddChaCheck[chaCheck] = true;
 						if (cNow != N - 1)
 							btk(rNow, cNow + 1, cnt + 1);
 						else
 							btk(rNow + 1, 0, cnt + 1);
-						visited[rNow][cNow] = false;
 						oddSumCheck[sumCheck] = false;
 						oddChaCheck[chaCheck] = false;
 					}
@@ -120,19 +107,17 @@ public class Main {
 				for (int r = rNow + 1; r < N; r++) {
 					for (int c = 0; c < N; c++) {
 						
-						int sumCheck = rNow + cNow;
-						int chaCheck = rNow - cNow + 10;
+						int sumCheck = r + cNow;
+						int chaCheck = r - cNow + 10;
 						
-						if (!visited[rNow][cNow] && !oddSumCheck[sumCheck] && !oddChaCheck[chaCheck]) {
+						if (!oddSumCheck[sumCheck] && !oddChaCheck[chaCheck]) {
 							
-							visited[rNow][cNow] = true;
 							oddSumCheck[sumCheck] = true;
 							oddChaCheck[chaCheck] = true;
 							if (cNow != N - 1)
 								btk(r, cNow + 1, cnt + 1);
 							else
 								btk(r + 1, 0, cnt + 1);
-							visited[rNow][cNow] = false;
 							oddSumCheck[sumCheck] = false;
 							oddChaCheck[chaCheck] = false;
 						}
