a2db4ee18f3e08dbe4ed04cfeacff011e0e6698c
dhkswls1998 <dhkswls1998@naver.com>
2024-10-24 22:50:43 +0900

    241024

--- ssafy-note/src/baekjoon/Main.java
+++ ssafy-note/src/baekjoon/Main.java
@@ -3,9 +3,6 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,55 +10,55 @@ public class Main {
 	static int[] dr = {0, 1, 1};
 	static int[] dc = {1, 1, 0};
 	
+	static int N;
+	static int[][] map;
+	static long[][][] dp;
+	
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 		
-		int[][] map = new int[N][N];
+		map = new int[N][N];
 		for (int r = 0; r < N; r++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int c = 0; c < N; c++)
 				map[r][c] = Integer.parseInt(st.nextToken());
 		}
 		
-		int[][] visited = new int[N][N];
-		Queue<int[]> bfs = new LinkedList<>();
-		bfs.add(new int[] {0, 1, 0});
+		dp = new long[N][N][3];
+		dp[0][1][0] = 1;
 		
-		while (!bfs.isEmpty()) {
-			
-			int[] now = bfs.poll();
-			int rNow = now[0];
-			int cNow = now[1];
-			int dWay = now[2];
-			
-			visited[rNow][cNow]++;
-
-			System.out.println("now: " + Arrays.toString(now));
-			for (int r = 0; r < N; r++)
-				System.out.println(Arrays.toString(visited[r]));
-			System.out.println();
-			
-			for (int d = 0; d < 3; d++) {
+		for (int r = 0; r < N; r++) {
+			for (int c = 2; c < N; c++) {
 				
-				if (dWay == 0) {
-					if (d == 2) continue;
-				} else if (dWay == 1) {
-					if (d == 0) continue;
+				if (map[r][c] == 0) {
+					
+					// 현재 가로 = 이전 가로&대각
+					if (map[r][c-1] == 0) {
+						dp[r][c][0] += dp[r][c-1][0];
+						dp[r][c][0] += dp[r][c-1][2];
+					}
+					
+					if (r > 0) {
+						// 현재 세로 = 이전 세로&대각
+						if (map[r-1][c] == 0) {
+							dp[r][c][1] += dp[r-1][c][1];
+							dp[r][c][1] += dp[r-1][c][2];
+						}
+						
+						// 현재 대각 = 이전 가로&세로&대각
+						if (map[r][c-1] == 0 && map[r-1][c] == 0 && map[r-1][c-1] == 0) {
+							dp[r][c][2] += dp[r-1][c-1][0];
+							dp[r][c][2] += dp[r-1][c-1][1];
+							dp[r][c][2] += dp[r-1][c-1][2];
+						}
+					}
 				}
-				
-				int rNext = rNow + dr[d];
-				int cNext = cNow + dc[d];
-				
-				if (rNext < 0 || N <= rNext || cNext < 0 || N <= cNext ||
-						map[rNext][cNext] == 1) continue;
-				
-				bfs.add(new int[] {rNext, cNext, d});
 			}
 		}
 		
-		System.out.println(visited[N-1][N-1]);
+		System.out.println(dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2]);
 	}
 }
