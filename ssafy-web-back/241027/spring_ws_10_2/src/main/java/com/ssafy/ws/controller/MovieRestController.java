86d9b2a80537c149ef06766e8d1c42839f8980b2
dhkswls1998 <dhkswls1998@naver.com>
2024-10-28 05:43:45 +0900

    241028

--- ssafy-web-back/241027/spring_ws_10_2/src/main/java/com/ssafy/ws/controller/MovieRestController.java
+++ ssafy-web-back/241027/spring_ws_10_2/src/main/java/com/ssafy/ws/controller/MovieRestController.java
@@ -2,23 +2,19 @@ package com.ssafy.ws.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.ws.model.dto.Movie;
-import com.ssafy.ws.model.dto.User;
 import com.ssafy.ws.model.service.MovieService;
-import com.ssafy.ws.model.service.UserService;
 
 
 @RestController
@@ -26,37 +22,55 @@ import com.ssafy.ws.model.service.UserService;
 @CrossOrigin("*")
 public class MovieRestController {
 	
-	// GetMapping
-	// select(READ)
-	@GetMapping("/user/{id}")
-	public ResponseEntity<?> selectUser(@PathVariable String id) {
-		return null;
+	private final MovieService movieService;
+	
+	public MovieRestController(MovieService movieService) {
+		this.movieService = movieService;
 	}
 
 	// GetMapping
 	// search(READ)
 	@GetMapping("/movie/{title}")
-	public ResponseEntity<?> select(@PathVariable String title) {
-		return null;
+	public ResponseEntity<?> select(@PathVariable("title") String title) {
+		
+		List<Movie> movies = movieService.search(title);
+		
+		if (movies == null || movies.size() == 0)
+			return ResponseEntity.noContent().build();
+		
+		return ResponseEntity.ok(movies);
 	}
 
 	@GetMapping("/movie")
 	public ResponseEntity<?> selectAll() {
-		return null;
+		
+		List<Movie> movies = movieService.selectAll();
+		
+		if (movies == null || movies.size() == 0)
+			return ResponseEntity.noContent().build();
+		
+		return ResponseEntity.ok(movies);
 	}
 
 	// PostMapping
 	// insert(CREATE)
 	@PostMapping("/movie")
-	public ResponseEntity<?> insert(Movie movie, @RequestPart(required = false) MultipartFile file) {
-		return null;
+	public ResponseEntity<?> insert(@ModelAttribute Movie movie) {
+//	public ResponseEntity<?> insert(@RequestBody Movie movie) {
+		
+		int result = movieService.insert(movie);
+		
+		return new ResponseEntity<>(result, result == 1 ? HttpStatus.CREATED : HttpStatus.BAD_REQUEST);
 	}
 
 	// DeleteMapping
 	// delete(DELETE)
 	@DeleteMapping("/movie/{id}")
-	public ResponseEntity<?> delete(@PathVariable int id) {
-		return null;
+	public ResponseEntity<?> delete(@PathVariable("id") int id) {
+		
+		int result = movieService.delete(id);
+		
+		return new ResponseEntity<>(result, result == 1 ? HttpStatus.NO_CONTENT : HttpStatus.BAD_REQUEST);
 	}
 	
 }
