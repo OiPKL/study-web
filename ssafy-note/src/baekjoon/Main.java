cfb40ad9ee1de4134f03d080a82ceaac5bf98f32
오완진 <dhkswls1998@naver.com>
2024-10-30 10:51:12 +0900

    241030

--- ssafy-note/src/baekjoon/Main.java
+++ ssafy-note/src/baekjoon/Main.java
@@ -6,22 +6,5 @@ import java.util.Arrays;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		
-		StringBuilder sb = new StringBuilder();
-		
-		
-		int[] nums = new int[100];
-		
-		int idx = 0;
-		for (int i = 1; i <= 10; i++) {
-			for (int j = 1; j <= 10; j++) {
-				nums[idx++] = i * j;
-			}
-		}
-		
-		Arrays.sort(nums);
-		
-		for (int i = 0; i < 100; i++) {
-			sb.append(i).append(" ").
-		}
 	}
 }
