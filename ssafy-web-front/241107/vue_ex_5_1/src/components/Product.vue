6fba83907ef10f4e2a65e8df79ad308a6586c05c
오완진 <dhkswls1998@naver.com>
2024-11-07 18:00:32 +0900

    241107

---
+++ ssafy-web-front/241107/vue_ex_5_1/src/components/Product.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h3>{{ product.name }}</h3>
+        <p>가격: {{ product.price }}원</p>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        product: Object
+    })
+</script>
---
+++ ssafy-web-front/241107/vue_ex_5_2/src/components/ProductListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+    {{ product.name }} - {{ product.price }}원
+    <button @click="addToCart">장바구니 추가</button>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps({
+  product: Object
+})
+
+function addToCart() {
+  console.log(`${props.product.name} 추가완료!`)
+}
+</script>
