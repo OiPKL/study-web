718e88f7b277a194eb92a48231cc23bd6195ee28
dhkswls1998 <dhkswls1998@naver.com>
2024-11-08 21:56:52 +0900

    241108

--- ssafy-note/src/programmers/Solution.java
+++ ssafy-note/src/programmers/Solution.java
@@ -1,72 +1,48 @@
 package programmers;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
 	
-	static int N, W, targetIdx;
+	static int[] dr = {1, 0, 0, -1};
+	static int[] dc = {0, -1, 1, 0};
+	static char[] key = {'d', 'l', 'r', 'u'};
+	static int R, C, K, sr, sc, er, ec;
+	static String answer = "impossible";
 	
-    public int solution(String sString, String tString, String[] words) {
+	public String solution(int n, int m, int x, int y, int r, int c, int k) {
     	
-    	N = words.length;
-    	W = sString.length();
-    	targetIdx = -1;
-    	
-        for (int n = 0; n < N; n++)
-            if (words[n].equals(tString))
-                targetIdx = n;
-    	
-    	if (targetIdx == -1)
-    		return 0;
-    	
-    	Queue<int[]> bfs = new LinkedList<>();
-    	boolean[] visited = new boolean[N];
-    	
-    	bfs.add(new int[] {-1, 0});
-    	
-    	while (!bfs.isEmpty()) {
-    		
-    		int[] now = bfs.poll();
-    		int nowIdx = now[0];
-    		int cnt = now[1];
-    		
-            if (nowIdx == -1) {
-                for (int nextIdx = 0; nextIdx < N; nextIdx++) {
-                    
-                	if (check(sString, words[nextIdx])) {
-                    	
-                        bfs.add(new int[] {nextIdx, cnt + 1});
-                        visited[nextIdx] = true;
-                    }
-                }
-                
-            } else if (nowIdx == targetIdx) {
-                return cnt;
-                
-            } else {
-                for (int nextIdx = 0; nextIdx < N; nextIdx++) {
-
-                	if (visited[nextIdx]) continue;
-                    
-                    if (check(words[nowIdx], words[nextIdx])) {
-                        bfs.add(new int[] {nextIdx, cnt + 1});
-                        visited[nextIdx] = true;
-                    }
-                }
-            }
-    	}
-    	
-    	return 0;
+		R = n;
+		C = m;
+		K = k;
+		sr = x - 1;
+		sc = y - 1;
+		er = r - 1;
+		ec = c - 1;
+		
+    	btk(sr, sc, 0, new StringBuilder(), false);
+    	return(answer);
     }
     
-    static boolean check(String word1, String word2) {
+    static void btk(int rNow, int cNow, int cnt, StringBuilder sb, boolean isEnd) {
     	
-    	int cnt = 0;
-    	for (int w = 0; w < W; w++)
-    		if (word1.charAt(w) != word2.charAt(w))
-    			cnt++;
+    	if (cnt > K || isEnd) return;
     	
-    	return cnt == 1;
+    	if (rNow == er && cNow == ec && cnt == K) {
+    		answer = sb.toString();
+    		isEnd = true;
+    		return;
+    	}
+    	
+    	for (int d = 0; d < 4; d++) {
+    		
+    		int rNext = rNow + dr[d];
+    		int cNext = cNow + dc[d];
+    		
+    		if (rNext < 0 || R <= rNext || cNext < 0 || C <= cNext)
+    			continue;
+    		
+    		sb.append(key[d]);
+    		btk(rNext, cNext, cnt + 1, sb, isEnd);
+    		sb.deleteCharAt(sb.length() - 1);
+    	}
     }
 }
