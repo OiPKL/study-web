1e215541e5f45ecedb4cd7385a3d665a627ba9fc
오완진 <dhkswls1998@naver.com>
2024-11-08 17:57:48 +0900

    241108

--- ssafy-web-front/241108/vue_ex_6_2/src/App.vue
+++ ssafy-web-front/241108/vue_ex_6_2/src/App.vue
@@ -1,16 +1,18 @@
 
 <template>
-  <header>
-    <nav>
-      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-      <span> | </span>
-      <RouterLink :to="{ name: 'about' }">About</RouterLink>
-      <span> | </span>
-      <RouterLink :to="{ name: 'user', params:{username:username}}">User</RouterLink>
-    </nav>
-  </header>
-  <input v-model="username">
-  <RouterView />
+  <div>
+    <header>
+      <nav>
+        <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+        <span> | </span>
+        <RouterLink :to="{ name: 'about' }">About</RouterLink>
+        <span> | </span>
+        <RouterLink :to="{ name: 'user', params:{username:username}}">User</RouterLink>
+      </nav>
+    </header>
+    <input v-model="username">
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
--- ssafy-web-front/my-vue/day06/Vue_Day06_Router/vue-project/src/App.vue
+++ ssafy-web-front/my-vue/day06/Vue_Day06_Router/vue-project/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
-    <h2>App.vue</h2>
+    <div>
+      <h2>App.vue</h2>
+    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink> | 
+      <RouterLink to="/about">About</RouterLink> | 
+      <RouterLink :to="{name: 'home'}">HomeName</RouterLink> | 
+      <RouterLink :to="{name: 'about'}">AboutName</RouterLink> | 
+      <RouterLink :to="{name: 'user', params: {id: 'ssafy1'}}">User1</RouterLink> | 
+      <RouterLink :to="{name: 'user', params: {id: 'ssafy2'}}">User2</RouterLink> | 
+      <RouterLink :to="{name: 'login'}">Login</RouterLink> | 
+      <RouterLink :to="{name: 'board'}">Board</RouterLink>
+    </nav>
+  
+    <RouterView/>
   </div>
-  <nav>
-    <RouterLink to="/">Home</RouterLink> | 
-    <RouterLink to="/about">about</RouterLink> | 
-    <RouterLink :to="{name: 'home'}">HomeName</RouterLink> | 
-    <RouterLink :to="{name: 'about'}">aboutName</RouterLink> | 
-    <RouterLink :to="{name: 'user', params: {id: 'ssafy1'}}">User1</RouterLink> | 
-    <RouterLink :to="{name: 'user', params: {id: 'ssafy2'}}">User2</RouterLink> | 
-    <RouterLink :to="{name: 'login'}">Login</RouterLink> | 
-    <RouterLink :to="{name: 'board'}">Board</RouterLink>
-  </nav>
-
-  <RouterView/>
 </template>
 
 <script setup>
