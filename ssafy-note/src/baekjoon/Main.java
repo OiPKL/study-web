3f80ae7c75f561f87f901de5770789be248ffac6
dhkswls1998 <dhkswls1998@naver.com>
2024-11-18 00:05:34 +0900

    241117

--- ssafy-note/src/baekjoon/Main.java
+++ ssafy-note/src/baekjoon/Main.java
@@ -3,89 +3,58 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static StringBuilder sb = new StringBuilder();
-	static boolean[][] garo, sero, nemo;
-	static int[][] map;
-	static boolean checked = false;
-	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		garo = new boolean[9][10];	// r
-		sero = new boolean[9][10];	// c
-		nemo = new boolean[9][10];	// 3 * (r/3) + (c/3)
-
-		map = new int[9][9];
-		for (int r = 0; r < 9; r++) {
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int c = 0; c < 9; c++) {
-				map[r][c] = Integer.parseInt(st.nextToken());
-				
-				if (map[r][c] != 0) {
-					
-					garo[r][map[r][c]] = true;
-					sero[c][map[r][c]] = true;
-					nemo[3 * (r/3) + (c/3)][map[r][c]] = true;
-				}
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(br.readLine());
+		
+		int[][][] map = new int[N + 1][M + 1][3];
+		for (int n = 1; n <= N; n++) {
+			String line = br.readLine();
+			for (int m = 1; m <= M; m++) {
+				char cell = line.charAt(m - 1);
+                if (cell == 'J')
+                    map[n][m][0] = 1;
+                else if (cell == 'O')
+                    map[n][m][1] = 1;
+                else
+                    map[n][m][2] = 1;
+                
+                for (int i = 0; i < 3; i++) {
+                    map[n][m][i] += (map[n - 1][m][i] + map[n][m - 1][i] - map[n - 1][m - 1][i]);
+                }
 			}
 		}
 		
-		btk(0, 0);
+        StringBuilder sb = new StringBuilder();
+		for (int k = 0; k < K; k++) {
+			
+			int[] S = new int[2];
+			int[] E = new int[2];
+			st = new StringTokenizer(br.readLine());
+			S[0] = Integer.parseInt(st.nextToken());
+			S[1] = Integer.parseInt(st.nextToken());
+			E[0] = Integer.parseInt(st.nextToken());
+			E[1] = Integer.parseInt(st.nextToken());
+			
+			sb.append(map[E[0]][E[1]][0] - map[S[0]-1][E[1]][0] - map[E[0]][S[1]-1][0] + map[S[0]-1][S[1]-1][0]);
+			sb.append(" ");
+			sb.append(map[E[0]][E[1]][1] - map[S[0]-1][E[1]][1] - map[E[0]][S[1]-1][1] + map[S[0]-1][S[1]-1][1]);
+			sb.append(" ");
+			sb.append(map[E[0]][E[1]][2] - map[S[0]-1][E[1]][2] - map[E[0]][S[1]-1][2] + map[S[0]-1][S[1]-1][2]);
+			sb.append("\n");
+		}
 		
 		System.out.println(sb);
-	}
-	
-	static void btk(int r, int c) {
-		
-		if (checked)
-			return;
-		
-		if (c == 9) {
-			r++;
-			c = 0;
-		}
-		
-		if (r == 9) {
-			
-			for (int rr = 0; rr < 9; rr++) {
-				for (int cc = 0; cc < 9; cc++)
-					sb.append(map[rr][cc]).append(" ");
-				sb.append("\n");
-			}
-			
-			checked = true;
-			return;
-		}
-		
-		if (map[r][c] == 0) {
-			
-			for (int num = 1; num <= 9; num++) {
-				
-				if (garo[r][num] || sero[c][num] || nemo[3 * (r/3) + (c/3)][num])
-					continue;
-				
-				map[r][c] = num;
-				garo[r][num] = true;
-				sero[c][num] = true;
-				nemo[3 * (r/3) + (c/3)][num] = true;
-				
-//				System.out.println(r + " | " + c + " | " + num);
-				
-				btk(r, c + 1);
-				
-				map[r][c] = 0;
-				garo[r][num] = false;
-				sero[c][num] = false;
-				nemo[3 * (r/3) + (c/3)][num] = false;
-			}
-		} else {
-			btk(r, c + 1);
-		}
 	}
 }
