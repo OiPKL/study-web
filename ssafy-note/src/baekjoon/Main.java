dc19c1c9d53c441753f7614ca94197ad389f34a9
OiPKL <dhkswls1998@naver.com>
2025-01-26 23:43:47 +0900

    250126

--- ssafy-note/src/baekjoon/Main.java
+++ ssafy-note/src/baekjoon/Main.java
@@ -3,54 +3,58 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int N, M;
-	static List<Integer> arr;
+	
+	static int N, K;
 	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
+	static int[] time;
 	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		arr = new ArrayList<>();
-		visited = new boolean[N+1];
-		
-		btk();
-		
-		System.out.println(sb);
-	}
-	
-	static void btk() {
-		
-		if (arr.size() == M) {
-			
-			for (int num : arr)
-				sb.append(num).append(" ");
-			sb.append("\n");
-			return;
-		}
-		
-		for (int i = 1; i <= N; i++) {
-			
-			if (!visited[i]) {
-				
-				visited[i] = true;
-				arr.add(i);
-				btk();
-				arr.remove(arr.size() - 1);
-				visited[i] = false;
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException{
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());
+    	K = Integer.parseInt(st.nextToken());
+    	
+    	visited = new boolean[100001];
+    	time = new int[100001];
+    	
+    	Deque<Integer> bfs = new LinkedList<>();
+    	bfs.add(N);
+    	visited[N] = true;
+    	time[N] = 0;
+    	
+        while (!bfs.isEmpty()) {
+        	
+        	int curr = bfs.pollFirst();
+        	
+        	if (curr * 2 <= 100000 && !visited[curr * 2]) {
+        		
+        		visited[curr * 2] = true;
+        		time[curr * 2] = time[curr];
+        		bfs.addFirst(curr * 2);
+        	}
+        	
+        	if (curr - 1 >= 0 && !visited[curr - 1]) {
+        		
+        		visited[curr - 1] = true;
+        		time[curr - 1] = time[curr] + 1;
+        		bfs.addLast(curr - 1);
+        	}
+        	
+        	if (curr + 1 <= 100000 && !visited[curr + 1]) {
+        		
+        		visited[curr + 1] = true;
+        		time[curr + 1] = time[curr] + 1;
+        		bfs.addLast(curr + 1);
+        	}
+        }
+        	
+        System.out.println(time[K]);
+    }
 }
