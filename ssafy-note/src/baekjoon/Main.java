6686e6ee511cbb74413cc211c9d090635ba15151
OiPKL <dhkswls1998@naver.com>
2025-03-06 22:15:48 +0900

    250306

--- ssafy-note/src/baekjoon/Main.java
+++ ssafy-note/src/baekjoon/Main.java
@@ -3,55 +3,79 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
+    static String[] led = {
+        "1111110",
+        "0110000",
+        "1101101",
+        "1111001",
+        "0110011",
+        "1011011",
+        "1011111",
+        "1110000",
+        "1111111",
+        "1111011" 
+    };
+    
     public static void main(String[] args) throws IOException {
     	
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
         
-        int[] arr = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        boolean[] visited = new boolean[N + 1];
-        
-        for (int i = 1; i <= N; i++) {
-            ArrayList<Integer> list = new ArrayList<>();
-            int current = i;
-            
-            while (!list.contains(current)) {
-                list.add(current);
-                current = arr[current];
-            }
-            
-            if (current == i) {
-                int idx = list.indexOf(current);
-                for (int j = idx; j < list.size(); j++) {
-                    visited[list.get(j)] = true;
-                }
+        int[][] diff = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+            	
+                int cost = 0;
+                for (int k = 0; k < 7; k++)
+                    if (led[i].charAt(k) != led[j].charAt(k))
+                        cost++;
+                
+                diff[i][j] = cost;
             }
         }
         
-        ArrayList<Integer> answer = new ArrayList<>();
-        for (int i = 1; i <= N; i++)
-            if (visited[i])
-                answer.add(i);
+        String xStr = formatZero(X, K);
+        int cnt = 0;
         
-        Collections.sort(answer);
+        for (int n = 1; n <= N; n++) {
+        	
+            if (n == X) continue;
+            
+            String checkStr = formatZero(n, K);
+            int totalCost = 0;
+            
+            for (int i = 0; i < K; i++) {
+                int xDigit = xStr.charAt(i) - '0';
+                int cDigit = checkStr.charAt(i) - '0';
+                totalCost += diff[xDigit][cDigit];
+            }
+            
+            if (totalCost >= 1 && totalCost <= P)
+                cnt++;
+        }
         
-        sb.append(answer.size()).append("\n");
-        for (int num : answer)
-            sb.append(num).append("\n");
+        System.out.println(cnt);
+    }
+    
+    static String formatZero(int x, int k) {
+        String str = Integer.toString(x);
+        char[] arr = new char[k];
+        int idx = 0;
+        int len = str.length();
+        
+        for (int i = 0; i < k - len; i++)
+            arr[idx++] = '0';
 
-        System.out.print(sb);
+        for (int i = 0; i < len; i++)
+            arr[idx++] = str.charAt(i);
+
+        return new String(arr);
     }
 }
