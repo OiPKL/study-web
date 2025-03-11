9b7545882bcea42bc571cc4ef99f307c932bec99
OiPKL <dhkswls1998@naver.com>
2025-03-11 22:48:44 +0900

    250311

--- ssafy-note/src/baekjoon/Main.java
+++ ssafy-note/src/baekjoon/Main.java
@@ -3,9 +3,9 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,40 +14,89 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int M = Integer.parseInt(st.nextToken()) - 1;
-		int N = Integer.parseInt(st.nextToken()) - 1;
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		
-		int[][] visited = new int[N][M];
+        boolean[][][] visited = new boolean[N][M][2];
 		
-		HashMap<Integer, Integer> hashmapN = new HashMap<>();
-		HashMap<Integer, Integer> hashmapM = new HashMap<>();
+		@SuppressWarnings("unchecked")
+		List<Integer>[] listN = new ArrayList[N];				// axis 1
+		@SuppressWarnings("unchecked")
+		List<Integer>[] listM = new ArrayList[M];				// axis 0
+		
+		for (int n = 0; n < N; n++)
+			listN[n] = new ArrayList<>();
+		for (int m = 0; m < M; m++)
+			listM[m] = new ArrayList<>();
 		
 		for (int k = 0; k < K; k++) {
 			st = new StringTokenizer(br.readLine());
-			int btnN = Integer.parseInt(st.nextToken()) - 1;
 			int btnM = Integer.parseInt(st.nextToken()) - 1;
-			
-			hashmapN.put(btnN, btnM);
-			hashmapM.put(btnM, btnN);
+			int btnN = Integer.parseInt(st.nextToken()) - 1;
+
+			listN[btnN].add(btnM);
+			listM[btnM].add(btnN);
 		}
 		
-		int time = 0;
-		int nNow = 0;
-		int mNow = 0;
-		boolean garo = false;	// true = btnN, false = btnM
+		// 종료지점 스위치 CASE
+        if (!listN[N-1].contains(M-1)) {
+            listN[N-1].add(M-1);
+            listM[M-1].add(N-1);
+        }
 		
-		Queue<int[]> bfs = new LinkedList<>();
-		bfs.add(new int[] {0, 0});
+		PriorityQueue<int[]> bfs = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+		bfs.add(new int[] {0, 0, 0, 0});
+		
+		// 시작지점 스위치 CASE
+		if (listN[0].contains(0))
+			bfs.add(new int[] {0, 0, 1, 1});
 		
 		while (!bfs.isEmpty()) {
 			
+			int[] now = bfs.poll();
+			int nNow = now[0];
+			int mNow = now[1];
+			int time = now[2];
+			int axis = now[3];
 			
+//			for (int i = 0; i <= 3; i++)
+//				System.out.print(now[i] + " ");
+//			System.out.println();
+			
+			if (visited[nNow][mNow][axis])
+				continue;
+			
+			visited[nNow][mNow][axis] = true;
+			
+			if (nNow == N - 1 && mNow == M -1) {
+				System.out.println(time - 1);
+				return;
+			}
+
+			if (axis == 0) {
+				
+				for (int nNext : listM[mNow]) {
+					
+					if (!visited[nNext][mNow][1]) {
+						
+						int tmp = time + Math.abs(nNext - nNow);
+						bfs.add(new int[] {nNext, mNow, tmp + 1, 1});
+					}
+				}
+			} else {
+				
+				for (int mNext : listN[nNow]) {
+					
+					if (!visited[nNow][mNext][1]) {
+						
+						int tmp = time + Math.abs(mNext - mNow);
+						bfs.add(new int[] {nNow, mNext, tmp + 1, 0});
+					}
+				}
+			}
 		}
 		
-		if (nNow == N - 1 && mNow == M - 1)
-			System.out.println(visited[nNow][mNow]);
-		else
-			System.out.println(-1);
+		System.out.println(-1);
 	}
 }
